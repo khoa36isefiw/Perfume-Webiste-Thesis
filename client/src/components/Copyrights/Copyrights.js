d9bd649ee08a21a849e72bbd6f3eb39b1138780f
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-08-16 16:28:14 +0700

    define copyrights

---
+++ client/src/components/Copyrights/Copyrights.js
@@ -0,0 +1,16 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
+
+function Copyrights() {
+    return (
+        <Box sx={{ p: 2, borderTop: '1px solid #333' }}>
+            {/* <Box sx={{ width: '100%', height: '1px', bgcolor: '#333' }} /> */}
+            <CustomizeTypography textAlign={'center'}>
+                &copy; 2024 Tomtoc. All rights reversed
+            </CustomizeTypography>
+        </Box>
+    );
+}
+
+export default Copyrights;
