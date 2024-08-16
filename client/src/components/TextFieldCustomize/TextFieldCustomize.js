55b72e5f7640049569a77e4bd0308e83bb94af4b
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-08-16 16:28:00 +0700

    define input textfield

---
+++ client/src/components/TextFieldCustomize/TextFieldCustomize.js
@@ -0,0 +1,46 @@
+import { TextField } from '@mui/material';
+import React from 'react';
+
+export const TextFieldCustomize = ({ inputValue, onChangeValue, placeholder, onHandleKeyDown }) => {
+    return (
+        <TextField
+            variant="outlined"
+            // fullWidth
+            placeholder={placeholder}
+            // value={cityName}
+            value={inputValue}
+            // value current value in text field
+            // onChange={(e) => setCityName(e.target.value)}
+            onChange={onChangeValue}
+            onKeyDown={onHandleKeyDown}
+            sx={{
+                '.MuiInputBase-root': {
+                    width: '220px',
+                    fontSize: '14px',
+                    height: '40px',
+                    color: 'white',
+                    borderTopLeftRadius: '12px',
+                    borderBottomLeftRadius: '12px',
+                    borderTopRightRadius: 0,
+                    borderBottomRightRadius: 0,
+                },
+                '& .MuiFormHelperText-root': {
+                    fontSize: '12.5px',
+                    color: 'red',
+                    mx: 1,
+                },
+                '& .MuiOutlinedInput-root': {
+                    '& fieldset': {
+                        borderColor: '#333',
+                    },
+                    '&:hover fieldset': {
+                        borderColor: '#333',
+                    },
+                    '&.Mui-focused fieldset': {
+                        borderColor: '#333',
+                    },
+                },
+            }}
+        />
+    );
+};
