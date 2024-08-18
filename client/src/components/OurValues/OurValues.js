61b4778ba29253fb05bce2a3ea05d723ed2fb4f4
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-08-18 20:24:20 +0700

    create ui

---
+++ client/src/components/OurValues/OurValues.js
@@ -0,0 +1,45 @@
+import { Avatar, Box, Container, Grid } from '@mui/material';
+import React from 'react';
+import ourValues from '../../assets/images/our_values.png';
+import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
+
+function OurValues() {
+    return (
+        <Box sx={{ height: '600px' }}>
+            <Grid container sx={{ display: 'flex', alignItems: 'center' }}>
+                <Grid item lg={6}>
+                    <Avatar
+                        src={ourValues}
+                        sx={{ height: '100%', width: '100%', borderRadius: 0 }}
+                        alt="Our Values"
+                    />
+                </Grid>
+                <Grid item lg={6} sx={{ p: 6 }}>
+                    <CustomizeTypography sx={{ textAlign: 'center', fontSize: '48px', mb: 4 }}>
+                        Our Values
+                    </CustomizeTypography>
+                    <CustomizeTypography
+                        sx={{ fontSize: '18px', textIndent: '48px', textAlign: 'justify', mb: 4 }}
+                    >
+                        At Local Face, our perfume retail store is built on a foundation of passion
+                        and authenticity. We believe in celebrating the individuality of every
+                        customer, providing a diverse collection of scents that resonate with their
+                        unique personality and style. Our dedicated team of fragrance enthusiasts is
+                        committed to creating a welcoming and inclusive environment, where
+                        connections are forged, and inspiration thrives.
+                    </CustomizeTypography>
+
+                    <CustomizeTypography
+                        sx={{ fontSize: '18px', textIndent: '48px', textAlign: 'justify' }}
+                    >
+                        Embracing sustainability and continuous learning, Local Face strives to be
+                        more than just a shopping destination; we are a community that inspires and
+                        empowers individuals on their fragrance journey.
+                    </CustomizeTypography>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
+
+export default OurValues;
