5b7a65276c3679c937a12f9487f92c8b7081b3b6
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-08-18 20:24:31 +0700

    continue doing home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import NewArrivals from '../components/NewArrivals/NewArrivals';
 import WelcomeHomePage from '../components/WelcomeHomePage/WelcomeHomePage';
+import OurValues from '../components/OurValues/OurValues';
+import BestSellingProducts from '../components/BestSellingProducts/BestSellingProducts';
 
 function HomePage() {
     return (
         <div>
             <NewArrivals />
             <WelcomeHomePage />
+            <OurValues />
+            <BestSellingProducts />
         </div>
     );
 }
