3fbc1af8c738903c76c7a14254c81b60d6e56fdd
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-08-19 22:46:50 +0700

    add routes for about us page

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,8 +1,12 @@
 import UserLayouts from '../layouts/UserLayout/UserLayouts';
+import AboutUs from '../pages/AboutUs';
 import HomePage from '../pages/HomePage';
 
 // not sign into this website
-const publicRoutes = [{ path: '/', component: HomePage, layout: UserLayouts }];
+const publicRoutes = [
+    { path: '/', component: HomePage, layout: UserLayouts },
+    { path: '/about-us', component: AboutUs, layout: UserLayouts },
+];
 
 // must signed into
 const privateRoutes = [{ path: '/signed-in', component: '', layout: '' }];
