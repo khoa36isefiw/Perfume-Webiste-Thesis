be3bb8a459e84c624695ff60a838fb2c48a73c77
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-08-19 09:49:19 +0700

    maybe: finshied best selling

--- client/src/components/BestSellingProducts/BestSellingProducts.js
+++ client/src/components/BestSellingProducts/BestSellingProducts.js
@@ -1,36 +1,41 @@
-import { Avatar, Box, Container, Grid } from '@mui/material';
+import { Avatar, Box, Container, Grid, IconButton } from '@mui/material';
 import React from 'react';
 import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
 import { theme } from '../../Theme/Theme';
+import Slider from 'react-slick';
 
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
+import bestSelling1 from '../../assets/images/bs_hp_1.png';
+import bestSelling2 from '../../assets/images/bs_hp_2.png';
+import bestSelling3 from '../../assets/images/bs_hp_3.png';
+import bestSelling4 from '../../assets/images/bs_hp_4.png';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 const perfumesData = [
     {
-        perfumeUrl:
-            'https://a.mktgcdn.com/p/U6cfqrozI8W0sA3pxU9T1WhZ1vgBwf0Fs9HdX-RUcL4/1080x1080.jpg',
+        perfumeUrl: bestSelling1,
         perfumeName: 'Golden',
         perfumePrice: '220.00',
         perfumeCapacity: '100',
     },
     {
-        perfumeUrl:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt34e0f_ziCdQfq5ElorCiPgX-yTvA_pv8NT9I2KVk-_RHRZfPtBa2timx3DzZEQ7lnKk&usqp=CAU',
+        perfumeUrl: bestSelling2,
         perfumeName: 'Owl',
         perfumePrice: '160.00',
         perfumeCapacity: '100',
     },
     {
-        perfumeUrl:
-            'https://media.cnn.com/api/v1/images/stellar/prod/220707134827-editors-perfume-missdior.jpg?q=w_1110,c_fill',
+        perfumeUrl: bestSelling3,
         perfumeName: 'Solutions',
         perfumePrice: '250.00',
         perfumeCapacity: '100',
     },
     {
-        perfumeUrl:
-            'https://a.mktgcdn.com/p/U6cfqrozI8W0sA3pxU9T1WhZ1vgBwf0Fs9HdX-RUcL4/1080x1080.jpg',
+        perfumeUrl: bestSelling4,
         perfumeName: 'Golden',
         perfumePrice: '220.00',
         perfumeCapacity: '100',
@@ -38,8 +43,33 @@ const perfumesData = [
 ];
 
 function BestSellingProducts() {
+    const settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nextArrow: <CustomizeNextArrow />,
+        prevArrow: <CustomizPreviousArrow />,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
+
     return (
-        <Container sx={{ minHeight: '50px', py: theme.spacingAxis.boxVerticalAxis }}>
+        <Container sx={{ height: '600px', py: theme.spacingAxis.boxVerticalAxis }}>
             <CustomizeTypography
                 sx={{
                     fontSize: '46px',
@@ -50,14 +80,15 @@ function BestSellingProducts() {
             >
                 Best Selling Products
             </CustomizeTypography>
-            {/* swiper */}
-            <Box sx={{ height: '600px' }}>
-                <Grid container spacing={2} sx={{ p: 2 }}>
+            <Box sx={{ height: '600px', mt: 4 }}>
+                <Slider {...settings}>
                     {perfumesData.map((perfume, index) => (
-                        <Grid item lg={3} md={3}>
-                            <Box
+                        <Box key={index} sx={{ p: 2, width: '180px' }}>
+                            <Grid
+                                container
                                 sx={{
-                                    bgcolor: '#333',
+                                    // background: `linear-gradient(${theme.palette.bestSelling}, ${theme.palette.bestSelling2})`,
+                                    background: theme.palette.bestSelling,
                                     borderRadius: 2,
                                     height: '450px',
                                     p: 1,
@@ -73,7 +104,12 @@ function BestSellingProducts() {
                                 />
                                 <CustomizeTypography
                                     textAlign={'center'}
-                                    sx={{ textAlign: 'center', mt: 2 }}
+                                    sx={{
+                                        textAlign: 'center',
+                                        my: 2,
+                                        fontSize: '18px',
+                                        fontWeight: 'bold',
+                                    }}
                                 >
                                     {perfume.perfumeName}
                                 </CustomizeTypography>
@@ -96,13 +132,47 @@ function BestSellingProducts() {
                                         {perfume.perfumeCapacity} ml
                                     </CustomizeTypography>
                                 </Box>
-                            </Box>
-                        </Grid>
+                            </Grid>
+                        </Box>
                     ))}
-                </Grid>
+                </Slider>
             </Box>
         </Container>
     );
 }
 
 export default BestSellingProducts;
+
+function CustomizeNextArrow({ onClick }) {
+    return (
+        <IconButton
+            onClick={onClick}
+            sx={{
+                position: 'absolute',
+                top: '50%',
+                right: '-30px',
+                transform: 'translateY(-50%)',
+                zIndex: 1,
+            }}
+        >
+            <ArrowForwardIosIcon sx={{ fontSize: '36px', color: '#fff' }} />
+        </IconButton>
+    );
+}
+
+function CustomizPreviousArrow({ onClick }) {
+    return (
+        <IconButton
+            onClick={onClick}
+            sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '-20px',
+                transform: 'translateY(-50%)',
+                zIndex: 1,
+            }}
+        >
+            <ArrowBackIosIcon sx={{ fontSize: '36px', color: '#fff' }} />
+        </IconButton>
+    );
+}
