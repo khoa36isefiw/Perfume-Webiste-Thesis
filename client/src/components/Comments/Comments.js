a98ba6296ca637e9a69a0bf2676c30b606743c04
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-08-24 18:12:59 +0700

    show user information who commented

--- client/src/components/Comments/Comments.js
+++ client/src/components/Comments/Comments.js
@@ -1,46 +1,92 @@
-import { Avatar, Box, Container, Divider, Rating } from '@mui/material';
+import { Avatar, Box, Container, Divider, IconButton, Rating } from '@mui/material';
 import React from 'react';
 import { theme } from '../../Theme/Theme';
 import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
-import CustomizeDivider from '../CustomizeDivider/CustomizeDivider';
+
+import { commentsData } from './commentsData';
+import VerifiedIcon from '@mui/icons-material/Verified';
 
 function Comments() {
     return (
         <Container sx={{ mt: 4 }}>
-            <Box sx={{ display: 'flex' }}>
-                {/* user image */}
-                <Avatar
-                    alt="user image"
-                    src={'https://letdiv.com/wp-content/uploads/2024/04/khoa-hoc-react-native.jpg'}
-                />
-                {/* name */}
-                <Box sx={{ ml: 2 }}>
-                    <CustomizeTypography sx={{ fontSize: '14px' }}>Shiina</CustomizeTypography>
-                    {/* stars, rating */}
-                    <Rating
-                        value={5}
-                        // MuiRating-root MuiRating-sizeMedium css-1qqgbpl-MuiRating-root
-                        sx={{
-                            fontSize: '18px',
-                            // change border color
-                            '& .MuiRating-iconEmpty .MuiSvgIcon-root': {
-                                color: theme.palette.thirth.main,
-                            },
-                            mb: 1,
-                        }}
-                    />
-                    <CustomizeTypography sx={{ fontSize: '14px', color: '#d9d9d9' }}>
-                        2024-07-31 11:11
-                    </CustomizeTypography>
-                    {/* content */}
-                    <CustomizeTypography>
-                        Hàng giá rẻ nên không đòi hỏi j nhiều, shop cb hàng nhanh
-                    </CustomizeTypography>
+            {commentsData.map((comment, index) => (
+                <Box key={index} sx={{ display: 'flex', mt: 2 }}>
+                    {/* user image */}
+                    <Avatar alt="user image" src={comment.userImage} />
+                    <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+                        {/* name */}
+                        <Box sx={{ ml: 2 }}>
+                            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                <CustomizeTypography sx={{ fontSize: '16px' }}>
+                                    {comment.userName}
+                                </CustomizeTypography>
+                                <IconButton>
+                                    <VerifiedIcon
+                                        sx={{
+                                            fontSize: '18px',
+                                            color: theme.palette.text.verified,
+                                            mb: 1,
+                                        }}
+                                    />
+                                </IconButton>
+                                <CustomizeTypography
+                                    sx={{
+                                        fontSize: '14px',
+                                        fontStyle: 'italic',
+                                        color: theme.palette.text.verified,
+                                    }}
+                                >
+                                    Đã mua hàng tại Tomtoc Perfumes
+                                </CustomizeTypography>
+                            </Box>
+                            {/* stars, rating */}
+                            <Rating
+                                readOnly
+                                value={5}
+                                // MuiRating-root MuiRating-sizeMedium css-1qqgbpl-MuiRating-root
+                                sx={{
+                                    fontSize: '18px',
+                                    // change border color
+                                    '& .MuiRating-iconEmpty .MuiSvgIcon-root': {
+                                        color: theme.palette.thirth.main,
+                                    },
+                                    mb: 1,
+                                }}
+                            />
+                            <CustomizeTypography sx={{ fontSize: '14px', color: '#d9d9d9' }}>
+                                {comment.timeCommented}
+                            </CustomizeTypography>
+                            {/* content */}
+                            <CustomizeTypography>{comment.content}</CustomizeTypography>
+                            <ShopResponse />
+                        </Box>
+                        {index !== commentsData.length - 1 && (
+                            <Divider sx={{ bgcolor: '#fff', my: 1 }} />
+                        )}
+                    </Box>
                 </Box>
-            </Box>
-            <Divider sx={{ bgcolor: '#fff', my: 1 }} />;
+            ))}
         </Container>
     );
 }
 
 export default Comments;
+
+const ShopResponse = () => {
+    return (
+        <Box
+            sx={{
+                width: '100%',
+                minheight: '50px',
+                bgcolor: '#333',
+                borderRadius: '8px',
+                p: 1,
+            }}
+        >
+            <CustomizeTypography sx={{ color: theme.palette.text.primary, fontWeight: 'bold' }}>
+                Shop's Response:
+            </CustomizeTypography>
+            <CustomizeTypography>Cảm ơn bạn đã mua hàng ủng hộ shop!</CustomizeTypography>
+        </Box>
+    );
+};
