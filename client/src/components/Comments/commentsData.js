619930cc1e3d2676805e1c1a60364a4259562d1f
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-08-24 18:13:20 +0700

    fake data for comments

---
+++ client/src/components/Comments/commentsData.js
@@ -0,0 +1,22 @@
+export const commentsData = [
+    {
+        userImage: 'https://letdiv.com/wp-content/uploads/2024/04/khoa-hoc-react-native.jpg',
+        userName: 'Shiina',
+        timeCommented: '2024-07-31 11:11',
+        content: 'Hàng giá rẻ nên không đòi hỏi j nhiều, shop cb hàng nhanh',
+    },
+    {
+        userImage:
+            'https://avatar-ex-swe.nixcdn.com/playlist/2023/04/26/e/a/d/0/1682501556032_500.jpg',
+        userName: 'Sound of Rain',
+        timeCommented: '2024-08-21 08:08',
+        content: 'Mình sẽ mua vào dịp khác, nước hoa của shop rất thơm và lưu hương rất lâu.',
+    },
+    {
+        userImage:
+            'https://i.ytimg.com/vi/DnHpC6oj4wE/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGDggQyh_MA8=&rs=AOn4CLAGuIKaEx0krwxFk0K-ZOTj7ixOyg',
+        userName: 'Tido Kang',
+        timeCommented: '2024-08-11 09:09',
+        content: 'Nước hoa rất thơm và giữ mùi rất lâu nha!',
+    },
+];
