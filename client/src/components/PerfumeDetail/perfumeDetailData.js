e93ead290230b4bd97e3b9727b44a217d29ca33c
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-08-24 18:13:46 +0700

    fake image for quick viewing image

---
+++ client/src/components/PerfumeDetail/perfumeDetailData.js
@@ -0,0 +1,6 @@
+export const quickViewImage = [
+    'https://orchard.vn/wp-content/uploads/2015/04/dior-homme-intense_1.jpg',
+    'https://orchard.vn/wp-content/smush-webp/2015/04/dior-homme-intense_2.jpg.webp',
+    'https://orchard.vn/wp-content/smush-webp/2015/04/dior-homme-intense_3.jpg.webp',
+    'https://orchard.vn/wp-content/smush-webp/2015/04/dior-homme-intense_4.jpg.webp',
+];
