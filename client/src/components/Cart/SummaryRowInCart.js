9e6853910081022db4340d7c5c98f5f0bd2cae85
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-08-26 17:45:06 +0700

    summary information for each price

---
+++ client/src/components/Cart/SummaryRowInCart.js
@@ -0,0 +1,42 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
+export const SummaryRowInCart = ({ label, value, isTotal, discount = null }) => {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+            }}
+        >
+            <CustomizeTypography
+                sx={{
+                    fontWeight: isTotal ? 'bold' : 'normal',
+                    fontSize: isTotal ? '18px' : '14px',
+                }}
+            >
+                {label}
+            </CustomizeTypography>
+
+            {discount ? (
+                <CustomizeTypography
+                    sx={{
+                        fontWeight: isTotal ? 'bold' : 'normal',
+                        fontSize: isTotal ? '18px' : '14px',
+                    }}
+                >
+                    {discount} - ${value}
+                </CustomizeTypography>
+            ) : (
+                <CustomizeTypography
+                    sx={{
+                        fontWeight: isTotal ? 'bold' : 'normal',
+                        fontSize: isTotal ? '18px' : '14px',
+                    }}
+                >
+                    ${value}
+                </CustomizeTypography>
+            )}
+        </Box>
+    );
+};
