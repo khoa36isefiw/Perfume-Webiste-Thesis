502e56fc31292b4a223261a5da57f0dcd3e5544e
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-08-27 22:25:16 +0700

    define page for checkout

---
+++ client/src/pages/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CheckoutInformation from '../components/CheckoutInformation/CheckoutInformation';
+
+function Checkout() {
+    return (
+        <div>
+            <CheckoutInformation />
+        </div>
+    );
+}
+
+export default Checkout;
