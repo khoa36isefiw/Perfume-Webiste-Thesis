2c03dddbb90d45bf8f659fef0f9e625dd6046d4a
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-08-28 22:13:35 +0700

    define textfield for credit card

--- client/src/components/TextFieldCustomize/TextFieldCustomize.js
+++ client/src/components/TextFieldCustomize/TextFieldCustomize.js
@@ -50,3 +50,65 @@ export const TextFieldCustomize = ({
         />
     );
 };
+
+export const CustomizeTextFieldCreditCard = ({
+    type,
+    name,
+    label,
+    placeholder,
+    value,
+    onChange,
+    onFocus,
+    variant,
+}) => {
+    return (
+        <TextField
+            fullWidth
+            type={type}
+            name={name}
+            label={label}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            onFocus={onFocus}
+            variant={variant}
+            // MuiFormLabel-root MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-shrink MuiInputLabel-sizeMedium MuiInputLabel-outlined MuiFormLabel-colorPrimary MuiFormLabel-filled MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-shrink MuiInputLabel-sizeMedium MuiInputLabel-outlined css-1jy569b-MuiFormLabel-root-MuiInputLabel-root
+            sx={{
+                // padding: '12.5px 13px',
+                '.MuiInputBase-root': {
+                    fontSize: '14px',
+                    color: 'white',
+                    borderRadius: 1,
+                },
+                '& .MuiFormHelperText-root': {
+                    fontSize: '14px',
+
+                    mx: 1,
+                },
+                '& .MuiOutlinedInput-root': {
+                    '& fieldset': {
+                        borderColor: '#d9d9d9',
+                    },
+                    '&:hover fieldset': {
+                        borderColor: '#d9d9d9',
+                    },
+                    '&.Mui-focused fieldset': {
+                        borderColor: '#d9d9d9',
+                    },
+                },
+                // MuiFormLabel-root MuiInputLabel-root
+                '.MuiFormLabel-root ': {
+                    color: 'white',
+                    fontSize: '12px',
+                },
+                '.MuiInputLabel-root': {
+                    color: 'white',
+                    fontSize: '12px',
+                },
+                '.MuiInputBase-input ': {
+                    padding: '13.5px 13px',
+                },
+            }}
+        />
+    );
+};
