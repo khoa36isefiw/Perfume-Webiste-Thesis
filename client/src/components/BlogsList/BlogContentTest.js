7db093b015c13ad0e869f4acd608e66163c965ce
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-03 23:59:14 +0700

    file to test ui

--- client/src/components/BlogsList/BlogContentTest.js
+++ client/src/components/BlogsList/BlogContentTest.js
@@ -1,7 +1,32 @@
 import React from 'react';
-import { Box, Container, IconButton, Typography } from '@mui/material';
+import { Box, Container, Grid, Typography } from '@mui/material';
 import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
 import { CustomizeTypographyBlog } from '../CustomizeTypography/CustomizeTypography';
+import { theme } from '../../Theme/Theme';
+
+const blogDetailData7 = [
+    {
+        blogTitle: 'Ingredient Highlight: Sesame Absolute',
+        blogImage:
+            'https://cdn.experimentalperfumeclub.com/wp-content/uploads/2022/11/EPC-Tonka-Sesame-50ml-sq.jpg',
+
+        blogContent: [
+            'Tonka Sesame might be the nutty scent you never knew you liked. Sesame is a popular ingredient in Asian cuisine, and offers a unique and exotic scent. Sesame-based fragrances typically feature notes of roasted sesame and honey. ',
+            'Tonka Sesame contains a wonderful ingredient – Sesame Absolute by LMR, which just smells like the sesame seeds you know. Only a trace in the formula has a powerful impact.',
+            'Our Tonka Sesame is a unique blend of tonka bean, sesame and hazelnut, resulting in a warm and nutty scent that is both comforting and sophisticated. Tonka Sesame is part of the Essential collection, which allows you to blend different fragrances to create a completely new unique scent. ',
+        ],
+    },
+    {
+        blogTitle: '',
+        blogImage:
+            'https://cdn.experimentalperfumeclub.com/wp-content/uploads/2022/11/Tonka-Extraordinaire-50ml-sqLR.jpg',
+
+        blogContent: [
+            'Tonka Extraordinaire is born from pairing two Essentials, Tonka Sesame with Amber Iris, creating a deeper, warmer,  fire-roasted quality. It’s a perfect pairing bringing together its original nutty facets with a warm ambery and tobacco drydown. ',
+            'Tonka Extraordinaire has been shortlisted for UK Fragrance Foundation’s Extraordinaire Category, a category that recognises independent perfumes for their extraordinary olfactive creations. ',
+        ],
+    },
+];
 
 function BlogContentTest() {
     return (
@@ -9,24 +34,24 @@ function BlogContentTest() {
             <Box
                 sx={{
                     backgroundColor: '#555',
-                    height: '650px',
+                    minHeight: '650px',
                     display: 'flex',
                     flexDirection: 'column',
                     alignItems: 'center',
                     justifyContent: 'center',
+                    py: 4,
                 }}
             >
                 <Box sx={{ maxWidth: 750 }}>
                     <Box
                         component="img"
-                        src="https://cdn.experimentalperfumeclub.com/wp-content/uploads/2023/12/Untitled-design-39.png"
+                        src="https://cdn.experimentalperfumeclub.com/wp-content/uploads/2023/05/maddi-bazzocco-UhrHTmVBzzE-unsplash-scaled.jpg"
                         alt="Blog Image"
                         sx={{
                             borderRadius: 0,
                             height: '550px',
                             width: '750px',
                             objectFit: 'cover',
-                            margin: '24px 0',
                         }}
                     />
                     <Typography
@@ -37,11 +62,12 @@ function BlogContentTest() {
                             fontFamily: 'Orator, Courier, sans-serif',
                         }}
                     >
-                        Why doesn’t my perfume last long enough?
+                        We are nuts about nutty scents
                     </Typography>
                 </Box>
             </Box>
 
+            {/* Quote */}
             <Container
                 sx={{
                     padding: '0 16px',
@@ -66,13 +92,13 @@ function BlogContentTest() {
                             fontStyle: 'italic',
                             fontFamily: 'Courier, sans-serif',
                             width: '60%',
+                            textAlign: 'center',
                         }}
                     >
-                        It is one of – if not the – most asked questions in perfumery. But, like all
-                        great questions, the answer is complicated.
-                        <br />
-                        <br />
-                        Here are five reasons you may not smell a fragrance for as long as you wish.
+                        When we talk gourmand notes in perfumery, we often think about caramel and
+                        vanilla but what about sweet notes that are not so excessively sweet. With
+                        woody and spicy undertones, let us introduce you to some delightful nutty
+                        scents.
                     </Typography>
                 </Box>
                 <Box
@@ -85,148 +111,91 @@ function BlogContentTest() {
                 >
                     <FormatQuoteIcon sx={{ fontSize: '32px', color: '#fff' }} />
                 </Box>
+            </Container>
 
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}
-                >
-                    <Box
-                        component="img"
-                        src="https://cdn.experimentalperfumeclub.com/wp-content/uploads/2023/12/Untitled-design-40.png"
-                        alt="Blog Image"
-                        sx={{
-                            borderRadius: 0,
-                            height: '550px',
-                            width: 750,
-                            objectFit: 'cover',
-                            margin: '24px 0',
-                        }}
-                    />
-                    <Typography
-                        sx={{
-                            fontSize: '26px',
-                            color: '#fff',
-                            maxWidth: 750,
-                            fontFamily: 'Courier, sans-serif',
-                        }}
+            <Box sx={{ my: 4, bgcolor: '#555' }}>
+                <Container>
+                    <Grid
+                        container
+                        spacing={2}
+                        sx={{ display: 'flex', alignItems: 'center', py: 4 }}
                     >
-                        1. Olfactory fatigue: Your brain is getting “blind” to your own scent.
-                    </Typography>
-                    <Box sx={{ maxWidth: 750, mt: 1 }}>
-                        <CustomizeTypographyBlog>
-                            Perfumers always talk about the olfactory pyramid, and for good reason.
-                            It provides the building blocks of any good perfume. But why? I hear you
-                            ask. The pyramid represents the stages in which fragrances evolve. Most
-                            perfume ingredients are volatile, meaning they have a shelf life.
-                        </CustomizeTypographyBlog>
-                        <CustomizeTypographyBlog>
-                            Perfumes must be constructed with volatile compounds (better known as
-                            VOC or volatile organic compounds) to be smelled and enjoyed in the
-                            first place. If they are volatile, this means your fragrance is bound to
-                            evaporate over time. Some ingredients (or molecules) evaporate quicker
-                            than others!
-                        </CustomizeTypographyBlog>
-                        <CustomizeTypographyBlog>
-                            The most volatile are those fresh-smelling citruses, aromatic and green
-                            notes. Each ingredient has an individual lifespan – generally speaking,
-                            top notes stay on the skin for up to 30 minutes.
-                        </CustomizeTypographyBlog>
-                        <CustomizeTypographyBlog>
-                            Heart notes – the ingredients that provide the core of any good perfume
-                            last around one to two hours. Mellow florals, spices and some gourmand
-                            notes dominate this time frame.
-                        </CustomizeTypographyBlog>
-                        <CustomizeTypographyBlog>
-                            Lastly, we have the base notes. These include rich scents such as woody
-                            (sandalwood, cedarwood), ambery (vanilla, labdanum, frankincense) and
-                            leather.
-                        </CustomizeTypographyBlog>
-                        <CustomizeTypographyBlog>
-                            If you love these deeper scents, your perfume probably lasts longer than
-                            the average. Stronger scents will have a robust sillage for others to
-                            enjoy (or hate!). It is common for these notes to last all day, at the
-                            very least, a few hours.
-                        </CustomizeTypographyBlog>
-                        <CustomizeTypographyBlog>
-                            On the contrary, if you favour fresh, citrusy and floral notes, you’ll
-                            find your fragrance lasts less, and you will need to re-spray throughout
-                            the day to continue enjoying your fragrance. Each of these categories of
-                            notes comes with its olfactory characteristics, which is very much down
-                            to personal preferences. Your skin chemistry also plays a significant
-                            part in how each ingredient behaves and how long it lasts. Very dry or
-                            oily skin can dampen the longevity of any fragrance, no matter the
-                            notes.
-                        </CustomizeTypographyBlog>
-                    </Box>
-                </Box>
-
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}
-                >
-                    <Box
-                        component="img"
-                        src="https://cdn.experimentalperfumeclub.com/wp-content/uploads/2023/12/Untitled-design-42.png"
-                        alt="Blog Image"
-                        sx={{
-                            borderRadius: 0,
-                            height: '550px',
-                            width: 750,
-                            objectFit: 'cover',
-                            margin: '24px 0',
-                        }}
-                    />
-                    <Typography
-                        sx={{
-                            fontSize: '26px',
-                            color: '#fff',
-                            maxWidth: 750,
-                            fontFamily: 'Courier, sans-serif',
-                        }}
-                    >
-                        3. Extract vs Eau de parfum vs Eau de toilette: Choose concentration
-                    </Typography>
-                    <Box sx={{ maxWidth: 750, mt: 1 }}>
-                        <CustomizeTypographyBlog>
-                            Buying a stronger concentration is an easy way to make your perfume last
-                            longer.
-                        </CustomizeTypographyBlog>
-                        <CustomizeTypographyBlog>
-                            Confusing an Extract with an Eau de toilette will leave you bitterly
-                            disappointed. But what’s the difference again?
-                        </CustomizeTypographyBlog>
-                        <CustomizeTypographyBlog>
-                            Your perfume is predominantly a mixture of two things – fragrance
-                            concentrate + alcohol.
-                        </CustomizeTypographyBlog>
-
-                        <CustomizeTypographyBlog>
-                            Eau de toilette is the least concentrated = typically around 10-12%
-                        </CustomizeTypographyBlog>
-                        <CustomizeTypographyBlog>
-                            Eau de parfum is more concentrated and usually one of the best choice =
-                            typically 20-25%
-                        </CustomizeTypographyBlog>
-                        <CustomizeTypographyBlog>
-                            Extract is the most concentrated = typically, over 25%
-                        </CustomizeTypographyBlog>
-                        <CustomizeTypographyBlog>
-                            While the choice of one of the above formats will have an impact on
-                            longevity, it also has an impact on the price you pay. Concentration is
-                            the main expense. Sometimes, the price tag can be eye-watering. By
-                            contrast, an EdT is the least pricey. EdPs fall in the middle, hence why
-                            most people (and perfumers) prefer Eau de Parfums.
-                        </CustomizeTypographyBlog>
-                    </Box>
-                </Box>
+                        <Grid item lg={12}>
+                            <CustomizeTypographyBlog
+                                sx={{
+                                    mb: 2,
+                                    fontSize: '24px',
+                                    color: theme.palette.text.secondary,
+                                    fontWeight: 'bold',
+                                }}
+                            >
+                                Three nutty notes that will give you salivating reactions:
+                            </CustomizeTypographyBlog>
+                            <CustomizeTypographyBlog>
+                                Tonka bean is a popular ingredient in gourmand fragrances and is
+                                known for its warm and spicy scent. Tonka bean fragrances typically
+                                contain notes of vanilla, caramel and almond. They offer a unique
+                                and complex scent that is perfect for those who enjoy a
+                                sophisticated gourmand.
+                            </CustomizeTypographyBlog>
+                            <CustomizeTypographyBlog>
+                                Pistachio is having its moment for those who love the small green
+                                nut. Pistachio cream fragrances typically contain notes of creamy
+                                vanilla, almond and of course, pistachio.
+                            </CustomizeTypographyBlog>
+                            <CustomizeTypographyBlog>
+                                Hazelnuts are another popular nut in gourmands. Fire-roasted
+                                hazelnuts, vanilla and caramel offer a warm and sweet scent,
+                                typically perfect for the fall or winter months.
+                            </CustomizeTypographyBlog>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+            <Container>
+                <Grid container spacing={4}>
+                    {blogDetailData7.map((blog, index) => (
+                        <Grid
+                            key={index}
+                            container
+                            spacing={4}
+                            sx={{ mt: 4, display: 'flex', alignItems: 'center' }}
+                            flexDirection={index % 2 === 0 ? 'row' : 'row-reverse'}
+                        >
+                            <Grid item xs={12} sm={6} md={4} lg={6} key={index}>
+                                <Box>
+                                    <CustomizeTypographyBlog
+                                        sx={{
+                                            fontSize: '32px',
+                                            fontWeight: 'bold',
+                                            mb: 2,
+                                        }}
+                                    >
+                                        {blog.blogTitle}
+                                    </CustomizeTypographyBlog>
+                                    {blog.blogContent?.map((text, index) => (
+                                        <CustomizeTypographyBlog
+                                            sx={{
+                                                textAlign: 'justify',
+                                                mb: 2,
+                                            }}
+                                        >
+                                            {text}
+                                        </CustomizeTypographyBlog>
+                                    ))}
+                                </Box>
+                            </Grid>
+                            <Grid item xs={12} sm={6} md={4} lg={6} key={index}>
+                                <Box
+                                    component={'img'}
+                                    alt="Blog Detail Image"
+                                    src={blog.blogImage}
+                                    sx={{ width: '100%', height: '600px' }}
+                                />
+                            </Grid>
+                        </Grid>
+                    ))}
+                </Grid>
             </Container>
         </Box>
     );
