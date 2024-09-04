5855166c10e88597bd2ae7f95b804e4366c21721
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-04 14:12:51 +0700

    change data for latest article in home page

--- client/src/components/LatestArticle/latestArticlesData.js
+++ client/src/components/LatestArticle/latestArticlesData.js
@@ -1,25 +1,25 @@
-import article_hp_1 from '../../assets/images/article_hp_1.png';
-import article_hp_2 from '../../assets/images/article_hp_2.png';
-import article_hp_3 from '../../assets/images/article_hp_3.png';
 export const latestArticlesData = [
     {
-        articleImage: article_hp_1,
-        articleTitle:
-            'The Soothing Symphony of Lavender Perfumes: Unlocking the Secrets of a Fragrant Elixir',
+        blogId: 7,
+        articleImage:
+            'https://cdn.experimentalperfumeclub.com/wp-content/uploads/2023/03/Instagram-post-portrait-1.png',
+        articleTitle: 'The best scents for spring',
         articleContent:
             'Lavender, with its enchanting aroma and rich history, has been cherished for centuries as a symbol of relaxation, healing, and timeless beauty. In the world of perfumery, lavender plays a key role in creating captivating fragrances loved by many.',
     },
     {
-        articleImage: article_hp_2,
-        articleTitle:
-            'The Art of Curating a Luxury Perfume Collection: A Symphony of Scents and Stories',
+        blogId: 8,
+        articleImage:
+            'https://cdn.experimentalperfumeclub.com/wp-content/uploads/2021/06/nathan-hurst-BgBTv96kEW0-unsplash-scaled.jpg',
+        articleTitle: 'Our Top Three Signature Blends for Summer',
         articleContent:
             "A luxury perfume collection is not just an assortment of fragrances; it is a reflection of one's taste, personality, and experiences. Each bottle holds a unique olfactory journey, crafted with the finest ingredients and artistic mastery.",
     },
     {
-        articleImage: article_hp_3,
-        articleTitle:
-            'The Timeless Elegance of Rose Perfumes: Unveiling the Queen of Flowers in Fragrance',
+        blogId: 9,
+        articleImage:
+            'https://cdn.experimentalperfumeclub.com/wp-content/uploads/2023/05/maddi-bazzocco-UhrHTmVBzzE-unsplash-scaled.jpg',
+        articleTitle: 'We are nuts about nutty scents',
         articleContent:
             'Rose, often referred to as the "Queen of Flowers," has held a special place in human culture and history for centuries. Beyond its captivating beauty, this iconic bloom has also inspired perfumers to create some of the most timeless and exquisite fragrances in the world.',
     },
