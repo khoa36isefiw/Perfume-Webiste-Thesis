35743878ffc36f898807b82ec037e8ee3715c117
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-05 21:12:58 +0700

    define for appearing animation

--- client/src/components/AnimationEffects/AnimationEffects.js
+++ client/src/components/AnimationEffects/AnimationEffects.js
@@ -5,3 +5,15 @@ export const fadeInAnimation = {
     },
     animation: 'fadeIn 2.5s ease-in-out',
 };
+
+export const scrollAppearingAnimation = {
+    '@keyframes appear': {
+        from: { opacity: 0, transform: 'translateY(100px)' },
+        to: { opacity: 1, transform: 'translateY(0)' },
+    },
+    // apply animation
+    animation: 'appear 5s linear',
+    // happeans only when the particular element is visible on the screen
+    animationTimeline: 'view()',
+    animationRange: 'entry 30% cover 60%',
+};
