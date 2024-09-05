c5f1272b9fb4e516db2aace5b113d3708fa5b11c
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-05 20:38:14 +0700

    add animation for text appearing in home page

--- client/src/components/NewArrivals/NewArrivals.js
+++ client/src/components/NewArrivals/NewArrivals.js
@@ -3,6 +3,7 @@ import NewArrival from '../../assets/images/homepage_new_arrivals.png';
 import { Box, Container, Grid } from '@mui/material';
 import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
 import CustomizeButton from '../CustomizeButton/CustomizeButton';
+import { fadeInAnimation } from '../AnimationEffects/AnimationEffects';
 
 function NewArrivals() {
     return (
@@ -10,14 +11,42 @@ function NewArrivals() {
             <Grid container sx={{ display: 'flex', alignItems: 'center' }}>
                 <Grid item lg={8}>
                     <Box sx={{ mb: 4 }}>
-                        <CustomizeTypography sx={{ fontWeight: 'bold', fontSize: '56px' }}>
+                        <CustomizeTypography
+                            sx={{
+                                fontWeight: 'bold',
+                                fontSize: '56px',
+                                ...fadeInAnimation,
+                            }}
+                        >
                             Elevate Your Spirit with Victory Scented Fragrances!
                         </CustomizeTypography>
-                        <CustomizeTypography fontSize="24px" sx={{ my: 2, width: '550px' }}>
+                        <CustomizeTypography
+                            fontSize="24px"
+                            sx={{
+                                my: 2,
+                                width: '550px',
+                                animation: 'fadeIn 2.5s ease-in-out',
+                                '@keyframes fadeIn': {
+                                    from: { opacity: 0, transform: 'translateY(20px)' },
+                                    to: { opacity: 1, transform: 'translateY(0)' },
+                                },
+                            }}
+                        >
                             Shop now and embrace the sweet smell of victory with Local Face.
                         </CustomizeTypography>
                     </Box>
-                    <CustomizeButton textAction={'Shop Now'} />
+                    <Box
+                        sx={{
+                            animation: 'fadeIn 2.5s ease-in-out',
+                            // define animation
+                            '@keyframes fadeIn': {
+                                from: { opacity: 0, transform: 'translateY(20px)' },
+                                to: { opacity: 1, transform: 'translateY(0)' },
+                            },
+                        }}
+                    >
+                        <CustomizeButton textAction={'Shop Now'} />
+                    </Box>
                 </Grid>
                 <Grid item lg={4}>
                     <Box
