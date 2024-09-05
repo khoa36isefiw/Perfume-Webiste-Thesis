ca8260dd8e4dddf7d8dce06c00a22c3d84eb6762
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-05 20:37:28 +0700

    define animations

---
+++ client/src/components/AnimationEffects/AnimationEffects.js
@@ -0,0 +1,7 @@
+export const fadeInAnimation = {
+    '@keyframes fadeIn': {
+        from: { opacity: 0, transform: 'translateY(20px)' },
+        to: { opacity: 1, transform: 'translateY(0)' },
+    },
+    animation: 'fadeIn 2.5s ease-in-out',
+};
