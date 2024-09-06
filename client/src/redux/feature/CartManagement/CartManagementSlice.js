a5ee8d6ac7cc7795254abf096425b41421719369
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-06 22:42:47 +0700

    create redux for cart management

---
+++ client/src/redux/feature/CartManagement/CartManagementSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    productInfor: [],
+};
+
+export const cartManagementSlice = createSlice({
+    name: 'cartManagement',
+    initialState,
+
+    reducers: {
+        addToCart: (state, action) => {
+            console.log('state: ', state);
+            const product = action.payload;
+            const existingProduct = state.productInfor.find(
+                (item) => item.id === product.perfumeID,
+            );
+            if (existingProduct) {
+                // if product is exisited, increase the quantity
+                existingProduct.quantity += 1;
+            } else {
+                // does not exist, add product to cart
+                state.productInfor.push({ ...product, quantity: 1 });
+            }
+        },
+        removeProduct: (state, action) => {
+            const productId = action.payload;
+            state.productInfor = state.productInfor.filter((item) => item.id === productId);
+        },
+        increaseQuantity: (state, action) => {
+            const productId = action.payload;
+            const existingProduct = state.productInfor.find((item) => item.id === productId);
+            if (existingProduct) {
+                existingProduct.quantity += 1;
+            }
+        },
+    },
+});
+
+const { actions, reducer } = cartManagementSlice;
+export const { addToCart, removeProduct, increaseQuantity } = actions; // named export
+export default reducer; // export default
