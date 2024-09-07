23779a8eb88d2f33f44ad1b9c1ee1e7844d7e081
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-07 23:29:17 +0700

    check is mobile screen at here

--- client/src/components/OurCollections/OurCollections.js
+++ client/src/components/OurCollections/OurCollections.js
@@ -1,33 +1,68 @@
 import { Box, Container, Grid } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
 import CustomizeTitle from '../CustomizeTitle/CustomizeTitle';
 import { groupCollection1, groupCollection2, groupCollection3 } from './collectionsData';
 import { scrollAppearingAnimationV2 } from '../AnimationEffects/AnimationEffects';
 
 function OurCollections() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    function handleWindowSizeChange() {
+        if (window.innerWidth < 739) {
+            setIsMobile(true);
+        }
+    }
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowSizeChange);
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        };
+    }, []);
     return (
         <Container>
             <CustomizeTitle heading={'Our Collections'} />
-            <Grid container spacing={4} sx={{ ...scrollAppearingAnimationV2 }}>
+            <Grid
+                container
+                spacing={4}
+                sx={
+                    {
+                        // ...scrollAppearingAnimationV2
+                    }
+                }
+            >
                 {groupCollection1.map((collection, index) => (
                     <ImageGridItem
                         key={index}
                         collection={collection}
-                        gridSize={index === 0 ? 4 : 8}
+                        gridSize={isMobile ? 12 : index === 0 ? 4 : 8}
                     />
                 ))}
             </Grid>
-            <Grid container spacing={4} sx={{ mt: 1, ...scrollAppearingAnimationV2 }}>
+            <Grid
+                container
+                spacing={4}
+                sx={{
+                    mt: 1,
+                    // ...scrollAppearingAnimationV2
+                }}
+            >
                 {groupCollection2.map((collection, index) => (
                     <ImageGridItem
                         key={index}
                         collection={collection}
-                        gridSize={index === 0 ? 6 : 6}
+                        gridSize={isMobile ? 12 : index === 0 ? 6 : 6}
                     />
                 ))}
             </Grid>
-            <Grid container spacing={4} sx={{ mt: 1, ...scrollAppearingAnimationV2 }}>
+            <Grid
+                container
+                spacing={4}
+                sx={{
+                    mt: 1,
+                    // ...scrollAppearingAnimationV2
+                }}
+            >
                 {groupCollection3.map((collection, index) => (
                     <ImageGridItem key={index} collection={collection} gridSize={4} />
                 ))}
@@ -40,7 +75,7 @@ export default OurCollections;
 
 function ImageGridItem({ collection, gridSize }) {
     return (
-        <Grid item lg={gridSize}>
+        <Grid item sm={gridSize} md={gridSize} lg={gridSize} sx={{ width: '100%' }}>
             <Box
                 sx={{
                     backgroundImage: `url(${collection.collectionImage})`,
