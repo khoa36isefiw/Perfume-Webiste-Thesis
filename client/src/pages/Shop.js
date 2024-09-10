17b443b2cc422827b25d84a5311f7d0eef67f23d
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-10 16:34:17 +0700

    remove something

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -2,13 +2,11 @@ import React from 'react';
 import PerfumesCard from '../components/PerfumesCard/PerfumesCard';
 import PerfumeBrands from '../components/PerfumeBrands/PerfumeBrands';
 import ShopPageInformation from '../components/ShopPageInformation/ShopPageInformation';
-import SortProducts from '../components/SortProducts/SortProducts';
 
 function Shop() {
     return (
         <div>
             <PerfumeBrands />
-            {/* <SortProducts /> */}
             <PerfumesCard />
             <ShopPageInformation />
         </div>
