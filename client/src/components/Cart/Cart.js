89c4cf81eddfb90215a526d03fb56bfd18f6cb88
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-11 18:55:26 +0700

    check if product list is not null

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -10,68 +10,75 @@ import { ProductInCart } from './ProductInCart';
 import { useSelector } from 'react-redux';
 import CartTotal from './CartTotal';
 import TotalPriceInCart from './TotalPriceInCart';
+import EmptyCart from '../EmptyCart/EmptyCart';
 
 function Cart() {
     const productAdded = useSelector((state) => state.cartManagement.productInfor);
     // console.log('productAdded', productAdded);
     const navigate = useNavigate();
     return (
-        <Container>
-            <Grid container spacing={2}>
-                <Grid item lg={12} xs={12}>
-                    <Button
-                        onClick={() => navigate('/shop')}
-                        startIcon={
-                            <ArrowBackIcon
+        <React.Fragment>
+            {productAdded.length > 0 ? (
+                <Container>
+                    <Grid container spacing={2}>
+                        <Grid item lg={12} xs={12}>
+                            <Button
+                                onClick={() => navigate('/shop')}
+                                startIcon={
+                                    <ArrowBackIcon
+                                        sx={{
+                                            fontSize: '24px',
+                                            color: '#fff',
+                                        }}
+                                    />
+                                }
                                 sx={{
-                                    fontSize: '24px',
+                                    fontSize: '16px',
+                                    textTransform: 'initial',
                                     color: '#fff',
+                                    fontWeight: 'normal',
+                                    transition:
+                                        'background-color 0.3s ease, color 0.3s ease, transform 0.3s ease',
+                                    '&:hover': {
+                                        transform: 'translateX(-10px)',
+                                        color: theme.palette.text.secondary,
+                                        fontWeight: 'bold',
+                                        // change color for icon
+                                        '& .MuiSvgIcon-root': {
+                                            color: theme.palette.text.secondary,
+                                        },
+                                    },
                                 }}
-                            />
-                        }
-                        sx={{
-                            fontSize: '16px',
-                            textTransform: 'initial',
-                            color: '#fff',
-                            fontWeight: 'normal',
-                            transition:
-                                'background-color 0.3s ease, color 0.3s ease, transform 0.3s ease',
-                            '&:hover': {
-                                transform: 'translateX(-10px)',
-                                color: theme.palette.text.secondary,
-                                fontWeight: 'bold',
-                                // change color for icon
-                                '& .MuiSvgIcon-root': {
-                                    color: theme.palette.text.secondary,
-                                },
-                            },
-                        }}
-                    >
-                        Continue Shopping
-                    </Button>
-                </Grid>
-                <Grid item lg={12}>
-                    <CustomizeTypography sx={{ fontSize: '48px', fontWeight: 'bold' }}>
-                        Your Cart
-                    </CustomizeTypography>
-                </Grid>
-                <Grid item lg={12}>
-                    <Divider sx={{ bgcolor: '#fff', my: 2 }} />
-                </Grid>
+                            >
+                                Continue Shopping
+                            </Button>
+                        </Grid>
+                        <Grid item lg={12}>
+                            <CustomizeTypography sx={{ fontSize: '48px', fontWeight: 'bold' }}>
+                                Your Cart
+                            </CustomizeTypography>
+                        </Grid>
+                        <Grid item lg={12}>
+                            <Divider sx={{ bgcolor: '#fff', my: 2 }} />
+                        </Grid>
 
-                <Grid container spacing={4} sx={{ ml: '-16px' }}>
-                    <Grid item xs={12} lg={8}>
-                        <ProductInCart productsList={productAdded} />
+                        <Grid container spacing={4} sx={{ ml: '-16px' }}>
+                            <Grid item xs={12} lg={8}>
+                                <ProductInCart productsList={productAdded} />
+                            </Grid>
+                            {/* <Grid item xs={12} lg={4}>
+                            <CartTotal productsList={productAdded} />
+                        </Grid> */}
+                            <Grid item xs={12} lg={4}>
+                                <TotalPriceInCart productsList={productAdded} />
+                            </Grid>
+                        </Grid>
                     </Grid>
-                    {/* <Grid item xs={12} lg={4}>
-                        <CartTotal productsList={productAdded} />
-                    </Grid> */}
-                    <Grid item xs={12} lg={4}>
-                        <TotalPriceInCart productsList={productAdded} />
-                    </Grid>
-                </Grid>
-            </Grid>
-        </Container>
+                </Container>
+            ) : (
+                <EmptyCart />
+            )}
+        </React.Fragment>
     );
 }
 
