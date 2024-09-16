8f606d26236d5e1658bedc63167528b001c07739
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-16 22:33:25 +0700

    create ui for who logged in

---
+++ client/src/components/AuthenticatedUser/AuthenticatedUser.js
@@ -0,0 +1,134 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import userDefaultAvatar from '../../assets/images/defaultA.png';
+import Logout from '@mui/icons-material/Logout';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import ManageHistoryIcon from '@mui/icons-material/ManageHistory';
+import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
+
+export default function AuthenticatedUser() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    return (
+        <React.Fragment>
+            <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+                <Tooltip
+                    title={
+                        <CustomizeTypography sx={{ fontSize: '13px', mb: 0 }}>
+                            Account settings
+                        </CustomizeTypography>
+                    }
+                >
+                    <IconButton
+                        onClick={handleClick}
+                        size="small"
+                        sx={{ ml: 2 }}
+                        aria-controls={open ? 'account-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                    >
+                        <Avatar
+                            sx={{ width: 48, height: 48 }}
+                            src={userDefaultAvatar}
+                            alt="User Image"
+                        />
+                    </IconButton>
+                </Tooltip>
+            </Box>
+            <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                slotProps={{
+                    paper: {
+                        elevation: 0,
+                        sx: {
+                            bgcolor: '#3D3D3D',
+                            overflow: 'visible',
+                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                            mt: 1.5,
+                            '& .MuiAvatar-root': {
+                                minWidth: 32,
+                                minHeight: 32,
+                                ml: -0.5,
+                                mr: 1,
+                            },
+                            '&::before': {
+                                content: '""',
+                                bgcolor: '#3D3D3D',
+                                display: 'block',
+                                position: 'absolute',
+                                top: 0,
+                                right: 14,
+                                width: 10,
+                                height: 10,
+                                transform: 'translateY(-50%) rotate(45deg)',
+                                zIndex: 0,
+                            },
+                        },
+                    },
+                }}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+                <MenuItem onClick={handleClose}>
+                    <Avatar
+                        sx={{ width: 40, height: 40 }}
+                        src={userDefaultAvatar}
+                        alt="User Image"
+                    />
+                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                        <CustomizeTypography sx={{ mb: 0, fontSize: '14px', fontWeight: 'bold' }}>
+                            Macbook
+                        </CustomizeTypography>
+
+                        <CustomizeTypography sx={{ mb: 0, fontSize: '14px', color: '#d5d5dd5' }}>
+                            macbook@gmail.com
+                        </CustomizeTypography>
+                    </Box>
+                </MenuItem>
+                <Divider sx={{ bgcolor: '#fff' }} />
+                <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                        <ManageAccountsIcon fontSize="large" sx={{ color: '#fff' }} />
+                    </ListItemIcon>
+                    <CustomizeTypography sx={{ mb: 0, fontSize: '14px' }}>
+                        My Account
+                    </CustomizeTypography>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                        <ManageHistoryIcon fontSize="large" sx={{ color: '#fff' }} />
+                    </ListItemIcon>
+                    <CustomizeTypography sx={{ mb: 0, fontSize: '14px' }}>
+                        My Purchase
+                    </CustomizeTypography>
+                </MenuItem>
+                <Divider sx={{ bgcolor: '#fff' }} />
+                <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                        <Logout fontSize="large" sx={{ color: '#fff' }} />
+                    </ListItemIcon>
+                    <CustomizeTypography sx={{ mb: 0, fontSize: '14px' }}>
+                        Logout
+                    </CustomizeTypography>
+                </MenuItem>
+            </Menu>
+        </React.Fragment>
+    );
+}
