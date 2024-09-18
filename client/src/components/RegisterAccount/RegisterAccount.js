d33cc37925c41351d4842856b473867412bf7c86
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-18 20:04:06 +0700

    handle for creating an account

--- client/src/components/RegisterAccount/RegisterAccount.js
+++ client/src/components/RegisterAccount/RegisterAccount.js
@@ -1,16 +1,80 @@
-import { Container, Grid } from '@mui/material';
-import React from 'react';
+import { Container, Grid, Box } from '@mui/material';
+import React, { useId, useRef, useState } from 'react';
 import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
 import { TextFieldLogin } from '../TextFieldCustomize/TextFieldCustomize';
 
 import { ipadProScreen, mobileScreen, tabletScreen, theme } from '../../Theme/Theme';
 import ButtonComponent from '../SignIn/test';
+import { useDispatch, useSelector } from 'react-redux';
+import { signUpAccount } from '../../redux/feature/AccountManagement/AccountManagementSlice';
+import NotificationMessage from '../NotificationMessage/NotificationMessage';
+import { useNavigate } from 'react-router-dom';
+import { backTop } from '../goBackTop/goBackTop';
 
 function RegisterAccount() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const userId = useId();
+    const listAccountRegistered = useSelector((state) => state.accountManagement.listAccounts);
+    console.log('listAccountRegistered: ', listAccountRegistered);
+    const firstNameRef = useRef(null);
+    const lastNameRef = useRef(null);
+    const phoneRef = useRef(null);
+    const addressRef = useRef(null);
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const [showNotification, setShowNotification] = useState(false);
+    const [showAnimation, setShowAnimation] = useState('animate__bounceInRight');
+    const [messageType, setMessageType] = useState('');
+    const [messageContent, setMessageContent] = useState('');
+    const [messageTitle, setMessageTitle] = useState('');
+
+    const handleRegisterAccount = () => {
+        // get value of first name input
+
+        const firstName = firstNameRef.current.value.trim();
+        const lastName = lastNameRef.current.value.trim();
+        const phoneNumber = phoneRef.current.value.trim();
+        const address = addressRef.current.value.trim();
+        const email = emailRef.current.value.trim();
+        const password = passwordRef.current.value.trim();
+        if (firstName) {
+            dispatch(
+                signUpAccount({
+                    userId,
+                    firstName,
+                    lastName,
+                    phoneNumber,
+                    address,
+                    email,
+                    password,
+                }),
+            );
+        }
+
+        setShowNotification(true);
+        setShowAnimation('animate__bounceInRight');
+        setMessageType('success');
+        setMessageTitle('Register account');
+        setMessageContent('Create new account successfully!');
+    };
+
+    const handleCloseNotification = () => {
+        setShowAnimation('animate__fadeOut');
+        setTimeout(() => {
+            setShowNotification(false);
+        }, 1000);
+    };
+
+    const handleNavigateSignIn = () => {
+        navigate('/sign-in');
+        backTop();
+    };
     return (
         <Container
             sx={{
                 width: '50%',
+                mt: 20,
                 [ipadProScreen]: {
                     width: '70%',
                     mt: 15,
@@ -63,25 +127,41 @@ function RegisterAccount() {
                                 <CustomizeTypography sx={{ color: theme.palette.text.secondary }}>
                                     First Name<span style={{ color: '#d14949' }}>*</span> :
                                 </CustomizeTypography>
-                                <TextFieldLogin placeholder="First Name" fullWidth />
+                                <TextFieldLogin
+                                    placeholder="First Name"
+                                    fullWidth
+                                    inputRef={firstNameRef}
+                                />
                             </Grid>
                             <Grid item xs={12} sm={6} lg={6}>
                                 <CustomizeTypography sx={{ color: theme.palette.text.secondary }}>
                                     Last Name<span style={{ color: '#d14949' }}>*</span> :
                                 </CustomizeTypography>
-                                <TextFieldLogin placeholder="Last Name" fullWidth />
+                                <TextFieldLogin
+                                    placeholder="Last Name"
+                                    fullWidth
+                                    inputRef={lastNameRef}
+                                />
                             </Grid>
                             <Grid item xs={12} sm={6} lg={6}>
                                 <CustomizeTypography sx={{ color: theme.palette.text.secondary }}>
                                     Phone Number<span style={{ color: '#d14949' }}>*</span> :
                                 </CustomizeTypography>
-                                <TextFieldLogin placeholder="Phone Number" fullWidth />
+                                <TextFieldLogin
+                                    placeholder="Phone Number"
+                                    fullWidth
+                                    inputRef={phoneRef}
+                                />
                             </Grid>
                             <Grid item xs={12} sm={6} lg={6}>
                                 <CustomizeTypography sx={{ color: theme.palette.text.secondary }}>
                                     Address<span style={{ color: '#d14949' }}>*</span> :
                                 </CustomizeTypography>
-                                <TextFieldLogin placeholder="Address" fullWidth />
+                                <TextFieldLogin
+                                    placeholder="Address"
+                                    fullWidth
+                                    inputRef={addressRef}
+                                />
                             </Grid>
                         </Grid>
 
@@ -100,14 +180,22 @@ function RegisterAccount() {
                                 <CustomizeTypography sx={{ color: theme.palette.text.secondary }}>
                                     E-mail<span style={{ color: '#d14949' }}>*</span> :
                                 </CustomizeTypography>
-                                <TextFieldLogin placeholder="E-mail" fullWidth />
+                                <TextFieldLogin
+                                    placeholder="E-mail"
+                                    fullWidth
+                                    inputRef={emailRef}
+                                />
                             </Grid>
 
                             <Grid item xs={12} sm={12} lg={12} sx={{ mt: 2, mb: '4px' }}>
                                 <CustomizeTypography sx={{ color: theme.palette.text.secondary }}>
                                     Password<span style={{ color: '#d14949' }}>*</span> :
                                 </CustomizeTypography>
-                                <TextFieldLogin placeholder="Password" fullWidth />
+                                <TextFieldLogin
+                                    placeholder="Password"
+                                    fullWidth
+                                    inputRef={passwordRef}
+                                />
                             </Grid>
                         </Grid>
                         <Grid
@@ -122,7 +210,10 @@ function RegisterAccount() {
                                 p: 2,
                             }}
                         >
-                            <ButtonComponent textAction={'Register Account'} />
+                            <ButtonComponent
+                                textAction={'Register Account'}
+                                onHandleClick={handleRegisterAccount}
+                            />
                         </Grid>
                         <Grid
                             item
@@ -142,11 +233,28 @@ function RegisterAccount() {
                             <CustomizeTypography sx={{ mb: 0, mr: 4 }}>
                                 Already have an account?
                             </CustomizeTypography>
-                            <ButtonComponent textAction={'Back to Sign In'} />
+                            <ButtonComponent
+                                textAction={'Back to Sign In'}
+                                onHandleClick={handleNavigateSignIn}
+                            />
                         </Grid>
                     </Grid>
                 </Grid>
             </Grid>
+            {showNotification && (
+                <Box
+                    sx={{ position: 'fixed', top: '5%', right: '1%', zIndex: 9999999 }}
+                    className={`animate__animated ${showAnimation}`}
+                >
+                    <NotificationMessage
+                        msgType={messageType}
+                        msgTitle={messageTitle}
+                        msgContent={messageContent}
+                        autoHideDuration={3000} // Auto-hide after 5 seconds
+                        onClose={handleCloseNotification}
+                    />
+                </Box>
+            )}
         </Container>
     );
 }
