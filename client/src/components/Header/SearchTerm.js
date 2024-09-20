a44f6cc55a42a6d492bf868767420a224f21b051
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-20 16:34:51 +0700

    create searching for mobile

---
+++ client/src/components/Header/SearchTerm.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { Box, IconButton } from '@mui/material';
+
+import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
+import SearchIcon from '@mui/icons-material/Search';
+
+import { mobileScreen, tabletScreen, theme } from '../../Theme/Theme';
+
+import { TextFieldCustomizeV2 } from '../TextFieldCustomize/TextFieldCustomize';
+
+function SearchTerm() {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                mt: 16,
+                p: 2,
+                [tabletScreen]: {
+                    mt: 2,
+                },
+            }}
+        >
+            <TextFieldCustomizeV2
+                // default
+                placeholder={'Search here...'}
+                sx={{
+                    width: '360px',
+                    [tabletScreen]: { width: '260px' },
+                    [mobileScreen]: {
+                        width: '100%',
+                    },
+                }}
+                // value={searchTerm}
+                // onChange={handleSearchChange}
+                // onClick={() => setShowSuggestions(!!searchTerm)}
+            />
+
+            <IconButton
+                sx={{
+                    bgcolor: theme.palette.text.secondary,
+                    borderTopLeftRadius: 1,
+                    borderBottomLeftRadius: 1,
+                    mr: 1,
+                    '&:hover': {
+                        bgcolor: theme.palette.text.secondary,
+                        cursor: 'pointer',
+                        fontWeight: 'bold',
+                    },
+                    [tabletScreen]: {
+                        mr: 4,
+                    },
+                }}
+            >
+                <SearchIcon sx={{ fontSize: '24px', color: 'white' }} />
+            </IconButton>
+        </Box>
+    );
+}
+
+export default SearchTerm;
