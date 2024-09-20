542a47807b6ac774dc30217013154b52c10d7582
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-20 23:40:01 +0700

    create container for checkout information

---
+++ client/src/components/CheckoutInformation/CheckoutContainer.js
@@ -0,0 +1,7 @@
+import { Box, styled } from '@mui/material';
+
+export const CheckoutContainer = styled(Box)(() => ({
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+}));
