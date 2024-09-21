89aa2218d57c4047b072ba551d432a53d6637e4d
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-21 21:48:23 +0700

    calculate total price in cart

--- client/src/components/Cart/TotalPriceInCart.js
+++ client/src/components/Cart/TotalPriceInCart.js
@@ -1,36 +1,26 @@
 import { Box } from '@mui/material';
-import React, { useState } from 'react';
+import React from 'react';
 import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
 import { useNavigate } from 'react-router-dom';
 import { CustomizeDividerV2 } from '../CustomizeDividerV2/CustomizeDividerV2';
-import Promocode from './Promocode';
+
 import { CustomizeButtonInCart } from '../CustomizeButtonInCart/CustomizeButtonInCart';
 import { SummaryRowInCart } from './SummaryRowInCart';
-import { calculateDiscount, calculateTax, converToVND } from '../convertToVND/convertToVND';
+import { converToVND } from '../convertToVND/convertToVND';
 
 function TotalPriceInCart({ productsList }) {
     const navigate = useNavigate();
 
-    // Calculate the total price including discount, tax, and promo code
+    // Calculate the total price
     const calculateTotal = () => {
         let totalSubtotal = 0;
-        let totalDiscount = 0;
-        let totalTax = 0;
-
-        // Loop through each product to calculate subtotal, discount, and tax
+        // Loop through each product to calculate subtotal
         productsList.forEach((product) => {
             const price = product.quantity * product.perfumePrice;
-            const discount = calculateDiscount(price);
-            const tax = calculateTax(price);
-
             totalSubtotal += price;
-            totalDiscount += discount;
-            totalTax += tax;
         });
 
-        let total = totalSubtotal - totalDiscount + totalTax;
-
-        return total;
+        return totalSubtotal;
     };
 
     return (
