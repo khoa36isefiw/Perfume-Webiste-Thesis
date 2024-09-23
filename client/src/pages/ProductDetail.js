7831b0fba905d57242ab8d862f16eee81157943d
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-23 23:40:45 +0700

    passing product data is selected

--- client/src/pages/ProductDetail.js
+++ client/src/pages/ProductDetail.js
@@ -4,14 +4,19 @@ import ProductInformation from '../components/ProductInformation/ProductInformat
 import CustomizeDivider from '../components/CustomizeDivider/CustomizeDivider';
 import RatingProduct from '../components/RatingProduct/RatingProduct';
 import Comments from '../components/Comments/Comments';
+import { useLocation } from 'react-router-dom';
 
 function ProductDetail() {
+    const location = useLocation();
+    // get the perfume data passed from navigation
+    const { perfume } = location.state || {};
+    console.log('perfume data at parent: ', perfume);
     return (
         <div>
             <PerfumeDetail />
             <CustomizeDivider />
             <ProductInformation />
-            <RatingProduct />
+            <RatingProduct perfumeDetailData={perfume} />
             <Comments />
         </div>
     );
