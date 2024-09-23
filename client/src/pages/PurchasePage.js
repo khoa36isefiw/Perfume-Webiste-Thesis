ebace36aaae59cb1d98d8adf5549ebe4e89ac6ac
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-23 22:29:20 +0700

    remove some redundant lines code

--- client/src/pages/PurchasePage.js
+++ client/src/pages/PurchasePage.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import MyPurchase from '../components/ProfileSettings/MyPurchase';
-import { useSelector } from 'react-redux';
-import { Box } from '@mui/material';
-import EmptyOrders from '../components/EmptyOrders/EmptyOrders';
 
 const PurchasePage = () => {
-    const loggedInAccount = useSelector((state) => state.accountManagement.loggedInAccount);
-    const orderHistory = useSelector(
-        // get for each user
-        (state) => state.checkoutManagement.listOrders[loggedInAccount?.userId] || [],
-    );
-    return <Box>{orderHistory.length > 0 ? <MyPurchase /> : <EmptyOrders />}</Box>;
+    return <MyPurchase />;
 };
 
 export default PurchasePage;
