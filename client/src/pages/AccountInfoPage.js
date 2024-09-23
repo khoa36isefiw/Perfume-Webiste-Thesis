092575b7086e977c4b7a004bcff9feb2f26b1895
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-23 12:34:48 +0700

    create profile settings page for authenticated

---
+++ client/src/pages/AccountInfoPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AccountInfo from '../components/ProfileSettings/AccountInfo';
+
+function AccountInfoPage() {
+    return (
+        <div>
+            <AccountInfo />
+        </div>
+    );
+}
+
+export default AccountInfoPage;
---
+++ client/src/pages/ChangePasswordPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChangePassword from '../components/ProfileSettings/ChangePassword';
+
+function ChangePasswordPage() {
+    return (
+        <div>
+            <ChangePassword />
+        </div>
+    );
+}
+
+export default ChangePasswordPage;
