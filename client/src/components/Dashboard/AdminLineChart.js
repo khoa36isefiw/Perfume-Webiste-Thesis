eb0b48bf9dba90bf6f8928dc2a0e62a75032d3de
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-26 12:20:34 +0700

    create total sales product by price

--- client/src/components/Dashboard/AdminLineChart.js
+++ client/src/components/Dashboard/AdminLineChart.js
@@ -1,18 +1,49 @@
 import * as React from 'react';
-import { LineChart } from '@mui/x-charts/LineChart';
+import { BarChart } from '@mui/x-charts/BarChart';
+import { AdminTypography } from '../CustomizeTypography/CustomizeTypography';
+import { Box } from '@mui/material';
+import { theme } from '../../Theme/Theme';
 
-export default function BasicArea() {
+const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+const xLabels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']; // Days of the week
+
+export default function TopSales() {
     return (
-        <LineChart
-            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-            series={[
-                {
-                    data: [2, 5.5, 2, 8.5, 1.5, 5],
-                    area: true,
-                },
-            ]}
-            width={500}
-            height={300}
-        />
+        <Box>
+            <AdminTypography
+                sx={{
+                    fontSize: '32px',
+                    fontWeight: 'bold',
+                    color: theme.palette.admin.primaryColor,
+                }}
+            >
+                Total Sales
+            </AdminTypography>
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <BarChart
+                    width={800}
+                    height={500}
+                    series={[{ data: pData, label: 'Total Sales', id: 'pvId' }]}
+                    xAxis={[{ data: xLabels, scaleType: 'band' }]}
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        '.MuiBarElement-root': {
+                            fill: '#49bc87',
+                        },
+                        '.MuiChartsLegend-mark': {
+                            fill: '#49bc87',
+                        },
+                    }}
+                />
+            </Box>
+        </Box>
     );
 }
