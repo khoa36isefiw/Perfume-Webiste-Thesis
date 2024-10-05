0deb0bedac2e7e6b28b9cb106e4d43c56488c2ad
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-10-05 07:08:16 +0700

    create format date

---
+++ client/src/components/FormatDate/formatDate.js
@@ -0,0 +1,31 @@
+export const formatDate = (inputDate) => {
+    // Convert input to a Date object if it's not already
+    const date = new Date(inputDate);
+
+    // Check if the date is valid
+    if (isNaN(date.getTime())) {
+        return 'Invalid date';
+    }
+
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero indexed
+    const year = date.getFullYear();
+
+    return `${month}/${day}/${year}`; // Return in mm/dd/yyyy format
+};
+
+export const formatDDMM = (inputDate) => {
+    // Convert input to a Date object if it's not already
+    const date = new Date(inputDate);
+
+    // Check if the date is valid
+    if (isNaN(date.getTime())) {
+        return 'Invalid date';
+    }
+
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero indexed
+    const year = date.getFullYear();
+
+    return `${year}/${month}/${day}`; // Return in dd/mm/yyyy format
+};
