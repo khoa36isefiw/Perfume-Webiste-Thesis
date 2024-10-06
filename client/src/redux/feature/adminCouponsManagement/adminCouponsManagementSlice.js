aee6f85d9b397e042a92640cbed9a83f066475b4
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-10-06 18:54:20 +0700

    create update coupon information feature

--- client/src/redux/feature/adminCouponsManagement/adminCouponsManagementSlice.js
+++ client/src/redux/feature/adminCouponsManagement/adminCouponsManagementSlice.js
@@ -14,9 +14,21 @@ export const adminCouponsManagementSlice = createSlice({
             console.log('data:', data);
             state.listCoupons = [...state.listCoupons, data];
         },
+        updateCoupon: (state, action) => {
+            const { couponId, data } = action.payload;
+            console.log('couponId: ', couponId);
+            console.log('data: ', data);
+            // get coupon index in list want to change their information
+            const couponIndex = state.listCoupons.findIndex((c) => c.id === couponId);
+
+            // update coupon information at this index
+            if (couponIndex !== -1) {
+                state.listCoupons[couponIndex] = { ...state.listCoupons[couponIndex], ...data };
+            }
+        },
     },
 });
 
 const { actions, reducer } = adminCouponsManagementSlice;
-export const { createNewCoupon } = actions; // named export
+export const { createNewCoupon, updateCoupon } = actions; // named export
 export default reducer; // export default
