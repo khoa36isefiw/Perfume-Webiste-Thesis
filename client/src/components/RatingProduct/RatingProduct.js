8e22d38f04fffe7ddb1507d52f3b3b748f7653e8
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-10-08 20:49:54 +0700

    handle for showing number of rating and average rating

--- client/src/components/RatingProduct/RatingProduct.js
+++ client/src/components/RatingProduct/RatingProduct.js
@@ -36,7 +36,7 @@ function RatingProduct({ perfumeDetailData }) {
         (state) => state.checkoutManagement.listOrders[loggedInAccount?.userId] || [],
     );
 
-    console.log('orderHistory: ', orderHistory);
+    // console.log('orderHistory: ', orderHistory);
 
     useEffect(() => {
         setHasCommented(true);
@@ -116,6 +116,23 @@ function RatingProduct({ perfumeDetailData }) {
         console.log('list commnets: ', comments);
     }, [comments]);
 
+    // returns the length of the list of comments based on the rating number
+    // trả về độ dài của list comments dựa trên rating number
+    const getRatingCount = (ratingNumber) => {
+        // return the list of users based on ratingNumber
+        return commentsList.filter((comment) => comment.ratingValue === ratingNumber).length;
+    };
+
+    console.log(commentsList.filter((comment) => comment.ratingValue === 5));
+
+    // calculating the average rating
+    const calculateAverageRating = () => {
+        if (commentsList.length === 0) return 0; // doesn't have rating
+        // calculate total of rating value from comment list
+        const totalRating = commentsList.reduce((acc, comment) => acc + comment.ratingValue, 0);
+        return (totalRating / commentsList.length).toFixed(1);
+    };
+
     return (
         <Container
             sx={{
@@ -178,7 +195,8 @@ function RatingProduct({ perfumeDetailData }) {
                                         },
                                     }}
                                 >
-                                    5.0
+                                    {/* 5.0 */}
+                                    {calculateAverageRating()}
                                 </CustomizeTypography>
                                 <StarIcon
                                     sx={{
@@ -216,57 +234,60 @@ function RatingProduct({ perfumeDetailData }) {
                         }}
                     >
                         <Box>
-                            {ratingData.map((rating, index) => (
-                                <Box sx={{ display: 'flex', alignItems: 'center' }} key={index}>
-                                    <CustomizeTypography>
-                                        {rating.numberOfRating}
-                                    </CustomizeTypography>
-                                    <StarIcon
-                                        sx={{
-                                            color: theme.palette.text.secondary,
-                                            fontSize: '24px',
-                                            mb: 1,
-                                            ml: 1,
-                                            [tabletScreen]: {
-                                                fontSize: '18px',
-                                            },
-                                        }}
-                                    />
-                                    <Box
-                                        sx={{
-                                            mb: 1,
-                                            ml: 1,
-                                            width: '150px',
-                                            height: '15px',
-                                            borderRadius: '4px',
-                                            bgcolor: theme.palette.text.secondary,
-                                            [ipadProScreen]: {
-                                                width: '120px',
-                                            },
-                                            [tabletScreen]: {
-                                                width: '100px',
-                                            },
-                                        }}
-                                    />
-                                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                        <CustomizeTypography sx={{ ml: 1 }}>
-                                            100%
+                            {ratingData.map((rating, index) => {
+                                const ratingCount = getRatingCount(rating.numberOfRating);
+                                return (
+                                    <Box sx={{ display: 'flex', alignItems: 'center' }} key={index}>
+                                        <CustomizeTypography>
+                                            {rating.numberOfRating}
                                         </CustomizeTypography>
+                                        <StarIcon
+                                            sx={{
+                                                color: theme.palette.text.secondary,
+                                                fontSize: '24px',
+                                                mb: 1,
+                                                ml: 1,
+                                                [tabletScreen]: {
+                                                    fontSize: '18px',
+                                                },
+                                            }}
+                                        />
                                         <Box
                                             sx={{
+                                                mb: 1,
+                                                ml: 1,
+                                                width: '150px',
                                                 height: '15px',
-                                                width: '1px',
+                                                borderRadius: '4px',
+                                                bgcolor:
+                                                    ratingCount > 0
+                                                        ? theme.palette.text.secondary
+                                                        : '#ccc', // rating
+
+                                                [ipadProScreen]: {
+                                                    width: '120px',
+                                                },
+                                                [tabletScreen]: {
+                                                    width: '100px',
+                                                },
+                                            }}
+                                        />
+
+                                        <Box
+                                            sx={{
+                                                width: '4px',
+                                                height: '1px',
                                                 bgcolor: '#fff',
-                                                mx: 1,
+                                                mx: 2,
                                                 mb: 1,
                                             }}
                                         />
-                                        <CustomizeTypography sx={{ ml: 1 }}>
-                                            2 đánh giá
+                                        <CustomizeTypography>
+                                            {ratingCount} đánh giá
                                         </CustomizeTypography>
                                     </Box>
-                                </Box>
-                            ))}
+                                );
+                            })}
                         </Box>
                     </Grid>
                     <Grid
