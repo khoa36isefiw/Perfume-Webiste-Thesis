5d04f634af50818909cffcfc561a97d56b013b2e
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-10-13 23:49:03 +0700

    handle for checking with paypal

--- client/src/components/PayPalButtonComponents/PayPalButtonComponents.js
+++ client/src/components/PayPalButtonComponents/PayPalButtonComponents.js
@@ -1,27 +1,72 @@
 import React from 'react';
 import { PayPalButtons } from '@paypal/react-paypal-js';
-function PayPalButtonsComponents() {
+function PayPalButtonsComponents({
+    loggedInAccount,
+    listProductInCart,
+    paymentMethod,
+    promoCodeApplied,
+    promoCode,
+}) {
     const handleCreateSubscription = () => {
         console.log('create subscription!');
     };
 
+    const finalTotal = () => {
+        const subtotal = listProductInCart.reduce(
+            (accumulator, product) => accumulator + product.quantity * product.perfumePrice,
+            0,
+        );
+
+        const calculateDiscount = (subtotal) => subtotal * 0.2; // 20%
+        const calculateTax = (subtotal) => subtotal * 0.1; // 10%
+
+        const discount = calculateDiscount(subtotal);
+        const tax = calculateTax(subtotal);
+
+        // final total: subtotal - discount + tax
+        let finalTotal = subtotal - discount + tax;
+
+        // optional: apply promotion code --> discount 5%
+        if (promoCodeApplied && promoCode === 'UTE99') {
+            finalTotal *= 0.95; // Apply 5% discount
+        }
+
+        // round final total to 2 decimal places
+        finalTotal = Math.round(finalTotal * 100) / 100;
+
+        // Check if a payment method is selected
+        return finalTotal;
+    };
+
+    const getTotalPrice = finalTotal();
+
     // tiến hành thanh toán
     const handleSubmitOrder = async () => {
-        // Check if a payment method is selected
+        // calculate for subtotal
 
         const order = {
-            // owner: user._id,
-            // items: cartItems.map((item) => ({
-            //     name: item.name,
-            //     price: item.price,
-            //     quantity: item.quantity,
-            //     size: item.size,
-            //     images: item.images,
-            // })),
-            // totalAmount: getTotalPriceVND(),
-            // paymentMethod: 'paypal',
-            // shippingFee: shippingFreeInVND,
-            // status: 'processing',
+            orderId: `${new Date().getTime()}`,
+            paymentMethod,
+            user: {
+                name: loggedInAccount?.firstName + ' ' + loggedInAccount?.lastName,
+                email: loggedInAccount?.email,
+                phone: loggedInAccount?.phoneNumber,
+                address: loggedInAccount?.address,
+            },
+            products: listProductInCart.map((product) => ({
+                productId: product.perfumeID,
+                name: product.perfumeName,
+                image: product.perfumeImage,
+                quantity: product.quantity,
+                size: product.perfumeSize,
+                price: product.quantity * product.perfumePrice,
+                brand: product.perfumeBrand,
+            })),
+            totalPrice: getTotalPrice,
+            // add timestamp for when the purchase was made
+            timestamp: new Date().toISOString(),
+            // test for commenting
+            isCommented: false,
         };
 
         console.log('information of order by PAYPAL: ', order);
@@ -52,14 +97,14 @@ function PayPalButtonsComponents() {
                 {
                     amount: {
                         currency_code: 'USD',
-                        // value: getTotalPrice(),
+                        value: getTotalPrice / 30000,
                     },
 
-                    // owner: user._id,
+                    owner: loggedInAccount.userId,
 
-                    // totalAmount: getTotalPrice(),
-                    paymentMethod: 'paypal',
-                    // shippingFee: shippingFreeInVND,
+                    totalAmount: getTotalPrice / 30000,
+                    paymentMethod: 'Paypal',
+                    shippingFee: 'free',
                     status: 'processing',
                 },
             ],
@@ -89,8 +134,8 @@ function PayPalButtonsComponents() {
                     color: 'blue',
                 }}
                 // createSubscription={handleCreateSubscription}
-                // onApprove={(data, actions) => {}}
-                // onCrreate={(data, actions) => {}}
+                createOrder={(data, actions) => createOrder(data, actions)}
+                onApprove={(data, actions) => onApprove(data, actions)}
                 amount="0.01"
                 // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
 
