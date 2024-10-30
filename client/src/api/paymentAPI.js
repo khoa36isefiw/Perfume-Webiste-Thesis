8dd5077128daaeaab3b7b94c04579e2f8c51d303
thoaihuynh0109 <thoaifamily83@gmail.com>
2024-10-30 17:56:14 +0700

    fix err

--- client/src/api/paymentAPI.js
+++ client/src/api/paymentAPI.js
@@ -2,11 +2,11 @@ import axiosClient from './axiosClient';
 
 export const paymentAPI = {
     createPayPalPayment: (user, items) => {
-        const url = '/payments/create-order';
+        const url = '/payments/paypal/create-order';
         return axiosClient.post(url, { user, items });
     },
     capturePayPalPayment: (paymentId) => {
-        const url = `/payments/capture-order`;
+        const url = `/payments/paypal/capture-order`;
         return axiosClient.post(url, { paymentId });
     },
 };
