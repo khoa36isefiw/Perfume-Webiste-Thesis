add91c68f18dfd7972fc700aca8134e363a6d01d
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-11-03 20:00:48 +0700

    add filter with brand params

--- client/src/components/PerfumeBrands/PerfumeBrands.js
+++ client/src/components/PerfumeBrands/PerfumeBrands.js
@@ -1,18 +1,32 @@
 import { Box, Container } from '@mui/material';
 import React, { useState } from 'react';
-import { brandsData } from './brandsData';
-import { ipadProScreen, mobileScreen, tabletScreen, theme } from '../../Theme/Theme';
 
-function PerfumeBrands({ listData, setSelectedBrandList }) {
+import { ipadProScreen, mobileScreen, tabletScreen, theme } from '../../Theme/Theme';
+import useBrand from '../../api/useBrand';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+function PerfumeBrands({ listData, brandSelected, setBrandSelected }) {
     console.log('listData: ', listData);
-    const [brandSelected, setBrandSelected] = useState('');
+    const { data: brands, isLoading, error } = useBrand();
+    const location = useLocation();
+    const navigate = useNavigate();
+    console.log('âhiahihi');
 
     const handleSelectBrand = (brand) => {
-        setBrandSelected(brand.brandName);
-        const filterList = listData.filter((item) => item.perfumeBrand === brand.brandName);
-        setSelectedBrandList(filterList);
-        console.log('filterList after filtering: ', filterList);
+        const brandName = brand.nameEn;
+        setBrandSelected(brandName);
+        window.localStorage.setItem('filter', JSON.stringify(brandName));
+        // Lấy query hiện tại mà không làm thay đổi mã hóa của nó
+        const currentQueryParams = new URLSearchParams(location.search);
+        // Xóa thương hiệu cũ (nếu có) và thêm thương hiệu mới
+        currentQueryParams.set('brand', brandSelected);
+        console.log('currentQueryParams.toString(): ', currentQueryParams.toString());
+
+        // Cập nhật URL với `navigate` mà không mã hóa thêm
+        navigate(`/shop?${currentQueryParams.toString()}`);
     };
+
+    console.log('brandSelected: ', brandSelected);
 
     return (
         // <Container
@@ -24,20 +38,22 @@ function PerfumeBrands({ listData, setSelectedBrandList }) {
                 flexWrap: 'wrap',
             }}
         >
-            {brandsData.map((brand, index) => (
+            {brands?.data.map((brand) => (
                 <Box
-                    key={index}
-                    component={'img'}
-                    alt={brand.brandName}
-                    src={brand.brandImage}
+                    key={brand._id}
                     onClick={() => handleSelectBrand(brand)}
                     sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        fontSize: '18px',
+                        fontWeight: 'bold',
                         bgcolor: '#fff',
                         height: '60px',
                         width: '140px',
                         borderRadius: '8px',
                         border:
-                            brandSelected === brand.brandName
+                            brandSelected === brand?.nameEn
                                 ? `4px solid ${theme.palette.secondaryText}`
                                 : '1px solid #333',
                         ml: 1,
@@ -69,8 +85,9 @@ function PerfumeBrands({ listData, setSelectedBrandList }) {
                             marginLeft: '3.5%',
                         },
                     }}
-                    lazy="loading"
-                />
+                >
+                    {brand.nameEn}
+                </Box>
             ))}
         </Box>
     );
