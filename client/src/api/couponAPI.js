0b999987d5830fc7bc85bbd59821bf524002b3e1
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-11-12 22:59:01 +0700

    add logic for delete and update

--- client/src/api/couponAPI.js
+++ client/src/api/couponAPI.js
@@ -5,4 +5,12 @@ export const couponAPI = {
         const url = '/promotions/';
         return axiosClient.post(url, data);
     },
+    deleteCoupon: (id) => {
+        const url = `/promotions/${id}/delete`;
+        return axiosClient.put(url);
+    },
+    updateCoupon: (id, data) => {
+        const url = `/promotions/${id}`;
+        return axiosClient.put(url, data);
+    },
 };
