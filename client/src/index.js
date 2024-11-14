3e1bce593d0e77163025d8bf83ab74166cd22d87
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-11-14 23:16:50 +0700

    add I18 Provider

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,9 @@ import { unstable_createMuiStrictModeTheme } from '@mui/material/styles';
 import { theme } from './Theme/Theme';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import i18n from './i18n';
+import { I18nextProvider } from 'react-i18next';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const mergeTheme = {
@@ -19,11 +22,13 @@ const mergeTheme = {
 root.render(
     <React.StrictMode>
         <CssBaseline>
-            <Provider store={store}>
-                <ThemeProvider theme={mergeTheme}>
-                    <App />
-                </ThemeProvider>
-            </Provider>
+            <I18nextProvider i18n={i18n} defaultNS={'translation'}>
+                <Provider store={store}>
+                    <ThemeProvider theme={mergeTheme}>
+                        <App />
+                    </ThemeProvider>
+                </Provider>
+            </I18nextProvider>
         </CssBaseline>
     </React.StrictMode>,
 );
