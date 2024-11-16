da95ce26ffe414391e61f548659980a74b9b532d
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-11-16 23:18:19 +0700

    fixed: error don't know blogId

--- client/src/pages/BlogDetail.js
+++ client/src/pages/BlogDetail.js
@@ -5,9 +5,11 @@ import { blogContent } from '../components/BlogsList/blogContent';
 
 function BlogDetail() {
     const location = useLocation();
-    const { blog } = location.state || {}; // Lấy dữ liệu blog từ state
+    const { blog } = location.state || {}; // get blog's data from the state
+    const blogDataSaved = JSON.parse(window.localStorage.getItem('blog_detail_data'));
+    console.log('blogDataSaved: ', blogDataSaved);
 
-    return <Box>{blogContent[blog.blogId]}</Box>;
+    return <Box>{blogContent[blogDataSaved.blogId]}</Box>;
 }
 
 export default BlogDetail;
