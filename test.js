ac360e9328d2dcd07db39d4d1eba62548e013381
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-11-17 01:04:57 +0700

    translating title for blog

--- test.js
+++ test.js
@@ -1,49 +1,40 @@
 const test = {
-    ourService: 'Our Services',
-    ourServiceText:
-        'At Tomtoc, we are dedicated to providing you with a delightful and immersive perfume shopping experience. Our services are tailored to ensure that you find the perfect fragrance that complements your unique personality and style. We take pride in offering a range of services that go beyond just selling perfumes, aiming to make your journey with us truly special.',
-    listServices: {
-        service1: {
-            title: 'Personal Fragrance Consultations',
-            content:
-                "Choosing the right fragrance can be a deeply personal experience. Our team of knowledgeable fragrance experts is here to guide you through this process. Whether you're looking for a signature scent, a gift for a loved one, or need assistance in exploring new fragrance families, our consultants will take the time to understand your preferences and suggest the perfect matches",
+    blogLists: {
+        blogKey1: {
+            title: 'Why doesn’t my perfume last long enough?',
+            content: '',
         },
-        service2: {
-            title: 'Custom Fragrance Creation',
-            content:
-                "Experience the art of bespoke perfumery with our custom fragrance creation service. Work closely with our skilled perfumers to craft a scent that is uniquely yours. From selecting individual notes to blending them into a harmonious symphony, we'll help you bring your fragrance vision to life. Whether it's a special occasion, a gift for someone dear, or simply a way to indulge in luxury, a custom-created perfume is an unforgettable experience.",
+        blogKey2: {
+            title: 'How to Choose your Perfume Palettes for the Changing Seasons',
+            content: '',
         },
-        service3: {
-            title: 'Fragrance Events and Workshops',
-            content:
-                'Join us for fragrance-centric events and workshops that celebrate the art of perfumery. Immerse yourself in the captivating world of scents, learn from experts, and discover the nuances of different fragrance families. These events are perfect for fragrance enthusiasts and novices alike, providing a unique opportunity to expand your olfactory knowledge.',
+        blogKey3: {
+            title: 'How to Make Your Bespoke Perfume',
+            content: '',
         },
-        service4: {
-            title: 'Online Shopping Convenience',
-            content:
-                'Explore our carefully curated collection of perfumes from the comfort of your home. Our user-friendly website offers a seamless online shopping experience, complete with detailed product descriptions and customer reviews. You can also reach out to our customer support team for any assistance during your shopping journey.',
+        blogKey4: {
+            title: 'How To Choose A Perfume For Someone Else',
+            content: '',
         },
-    },
-    ser1: 'Our mission is to empower individuals with knowledge and facilitate meaningful connections through our platform.',
-    ser2: "We understand the importance of reliable and up-to-date information in today's fast-paced world.",
-    listBenefits: {
-        bene1: {
-            title: 'Fast delivery',
-            content:
-                'The specific delivery time will vary depending on the shipping address and the selected delivery option. Customers can track their order online to see the estimated delivery date.',
+        blogKey5: {
+            title: 'Understand Your Fragrance Family And Find Your Perfect Match',
+            content: '',
         },
-        bene1: {
-            title: 'Fast delivery',
-            content:
-                'The specific delivery time will vary depending on the shipping address and the selected delivery option. Customers can track their order online to see the estimated delivery date.',
+        blogKey6: {
+            title: 'Our Best Perfume Books to Learn from Perfumers',
+            content: '',
         },
-        bene1: {
-            title: '24/7 support',
-            content:
-                'CMS Service support is available 24 hours a day, 7 days a week. You can reach them by phone, email, or chat. Here are the contact information for CMS Service support.',
+        blogKey7: {
+            title: 'The best scents for spring',
+            content: '',
+        },
+        blogKey8: {
+            title: 'Our Top Three Signature Blends for Summer',
+            content: '',
+        },
+        blogKey9: {
+            title: 'We are nuts about nutty scents',
+            content: '',
         },
     },
-    ser3: 'CMS Service support is available 24 hours a day, 7 days a week. You can reach them by phone, email, or chat. Here are the contact information for CMS Service support.',
-    ser4: "If you have any questions or need assistance, please do not hesitate to reach out to our friendly team. We're here to make your fragrance exploration a truly memorable one.",
-    ser5: 'Tomtoc Team.',
 };
