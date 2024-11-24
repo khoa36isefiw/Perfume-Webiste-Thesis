79d6dadcd0112e3cfad6b39e55fc8b748437eabf
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-11-25 00:33:11 +0700

    handle logic for filter by brand

--- client/src/components/OurCollections/OurCollections.js
+++ client/src/components/OurCollections/OurCollections.js
@@ -3,9 +3,14 @@ import React, { useEffect, useState } from 'react';
 import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
 import CustomizeTitle from '../CustomizeTitle/CustomizeTitle';
 import { groupCollection1, groupCollection2, groupCollection3 } from './collectionsData';
+import { useTranslation } from 'react-i18next';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function OurCollections() {
     const [isMobile, setIsMobile] = useState(false);
+    const { i18n } = useTranslation('translate');
+    const location = useLocation();
+    const navigate = useNavigate();
 
     function handleWindowSizeChange() {
         if (window.innerWidth < 739) {
@@ -18,12 +23,26 @@ function OurCollections() {
             window.removeEventListener('resize', handleWindowSizeChange);
         };
     }, []);
+
+    const handleSelectCollection = (brand) => {
+        window.localStorage.setItem('filter', JSON.stringify(brand.destination));
+        // get the current search query
+        const currentQueryParams = new URLSearchParams(location.search);
+        // remove the old brand and add new brand selected
+        currentQueryParams.set('brand', brand.destination);
+        console.log('currentQueryParams.toString(): ', currentQueryParams.toString());
+
+        // update url
+        navigate(`/${i18n.language}/shop?${currentQueryParams.toString()}`);
+    };
+
     return (
         <Container>
             <CustomizeTitle heading={'Top Brands'} />
             <Grid container spacing={4}>
                 {groupCollection1.map((collection, index) => (
                     <ImageGridItem
+                        onHandleClick={() => handleSelectCollection(collection)}
                         key={index}
                         collection={collection}
                         gridSize={isMobile ? 12 : index === 0 ? 4 : 8}
@@ -39,6 +58,7 @@ function OurCollections() {
             >
                 {groupCollection2.map((collection, index) => (
                     <ImageGridItem
+                        onHandleClick={() => handleSelectCollection(collection)}
                         key={index}
                         collection={collection}
                         gridSize={isMobile ? 12 : index === 0 ? 6 : 6}
@@ -53,7 +73,12 @@ function OurCollections() {
                 }}
             >
                 {groupCollection3.map((collection, index) => (
-                    <ImageGridItem key={index} collection={collection} gridSize={4} />
+                    <ImageGridItem
+                        onHandleClick={() => handleSelectCollection(collection)}
+                        key={index}
+                        collection={collection}
+                        gridSize={4}
+                    />
                 ))}
             </Grid>
         </Container>
