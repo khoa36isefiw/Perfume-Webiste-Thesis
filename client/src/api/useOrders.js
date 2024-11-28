0153639504b97111ca4bee10fa37e787ddd47a03
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-11-28 23:59:08 +0700

    redefine

--- client/src/api/useOrders.js
+++ client/src/api/useOrders.js
@@ -2,7 +2,7 @@ import useSWR from 'swr';
 import axiosClient from './axiosClient';
 
 export default function useOrders() {
-    const url = `/orders`;
+    const url = `/orders/`;
     const fetcher = (url) => axiosClient.get(url);
     const { data, mutate, isLoading, error } = useSWR(url, fetcher);
 
