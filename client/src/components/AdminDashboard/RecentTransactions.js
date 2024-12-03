fb63189ea07784b614098cec1e5e77a552998f85
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-12-03 22:53:20 +0700

    feat: add transaction data

--- client/src/components/AdminDashboard/RecentTransactions.js
+++ client/src/components/AdminDashboard/RecentTransactions.js
@@ -1,9 +1,12 @@
 import { Box, Divider } from '@mui/material';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     AdminHeadingTypography,
     AdminTypography,
 } from '../CustomizeTypography/CustomizeTypography';
+import useRecentTransation from '../../api/useRecentTransation';
+import { formatDate } from '../FormatDate/formatDate';
+import { converToVND } from '../convertToVND/convertToVND';
 
 const dataTitleTable = ['Name', 'Date', 'Amount', 'Status'];
 
@@ -67,6 +70,28 @@ const listUser = [
 ];
 
 function RecentTransactions() {
+    const { data: recentTransactions, isLoading } = useRecentTransation();
+    console.log('recentTransactions: ', recentTransactions?.data);
+    const [transactions, setTransactions] = useState([]);
+    //return an array object
+
+    useEffect(() => {
+        if (recentTransactions && recentTransactions?.data) {
+            const listUsers = recentTransactions?.data?.map((transaction) => {
+                return {
+                    id: transaction._id,
+                    name:
+                        transaction.order?.user.firstName + ' ' + transaction.order?.user.lastName,
+                    date: transaction.createdAt,
+                    amount: transaction.amount,
+                    status: transaction.paid,
+                };
+            });
+            setTransactions(listUsers);
+        }
+    }, [recentTransactions?.data]);
+
+    console.log('listUsers: ', transactions);
     return (
         <Box sx={{ padding: 2, borderRadius: 1, bgcolor: '#fff', border: '1px solid #d5d5d5' }}>
             <AdminHeadingTypography sx={{ fontSize: '24px', mb: 2 }}>
@@ -75,11 +100,12 @@ function RecentTransactions() {
 
             {/* Table Header */}
             <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                {dataTitleTable.map((title, index) => (
-                    <AdminTypography key={index} sx={{ flex: 1, textAlign: 'center' }}>
-                        {title}
-                    </AdminTypography>
-                ))}
+                {dataTitleTable.length > 0 &&
+                    dataTitleTable.map((title, index) => (
+                        <AdminTypography key={index} sx={{ flex: 1, textAlign: 'center' }}>
+                            {title}
+                        </AdminTypography>
+                    ))}
             </Box>
 
             {/* Divider */}
@@ -87,42 +113,43 @@ function RecentTransactions() {
 
             {/* Table Body */}
             <Box sx={{ maxHeight: '270px', overflow: 'scroll' }}>
-                {listUser.map((user, index) => (
-                    <Box key={user.id}>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'space-between',
-                                mb: 1,
-                            }}
-                        >
-                            <AdminTypography sx={{ flex: 1, textAlign: 'center' }}>
-                                {user.name}
-                            </AdminTypography>
-                            <AdminTypography sx={{ flex: 1, textAlign: 'center' }}>
-                                {user.date}
-                            </AdminTypography>
-                            <AdminTypography sx={{ flex: 1, textAlign: 'center' }}>
-                                {user.amount}
-                            </AdminTypography>
-                            <AdminTypography
+                {transactions.length > 0 &&
+                    transactions.map((user, index) => (
+                        <Box key={user.id}>
+                            <Box
                                 sx={{
-                                    flex: 1,
-                                    textAlign: 'center',
-                                    bgcolor: user.status === 1 ? '#ddfbe9' : '#f0ed1f87',
-                                    borderRadius: 2,
-                                    padding: '4px 0',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'space-between',
+                                    mb: 1,
                                 }}
                             >
-                                {user.status === 1 ? 'Paid' : 'Pending'}
-                            </AdminTypography>
+                                <AdminTypography sx={{ flex: 1, textAlign: 'center' }}>
+                                    {user.name}
+                                </AdminTypography>
+                                <AdminTypography sx={{ flex: 1, textAlign: 'center' }}>
+                                    {formatDate(user.date)}
+                                </AdminTypography>
+                                <AdminTypography sx={{ flex: 1, textAlign: 'center' }}>
+                                    {converToVND(user.amount)}
+                                </AdminTypography>
+                                <AdminTypography
+                                    sx={{
+                                        flex: 1,
+                                        textAlign: 'center',
+                                        bgcolor: user.status === true ? '#ddfbe9' : '#f0ed1f87',
+                                        borderRadius: 2,
+                                        padding: '4px 0',
+                                    }}
+                                >
+                                    {user.status === true ? 'Paid' : 'Pending'}
+                                </AdminTypography>
+                            </Box>
+                            <React.Fragment>
+                                {transactions.length - 1 !== index && <Divider sx={{ my: 1 }} />}
+                            </React.Fragment>
                         </Box>
-                        <React.Fragment>
-                            {listUser.length - 1 !== index && <Divider sx={{ my: 1 }} />}
-                        </React.Fragment>
-                    </Box>
-                ))}
+                    ))}
             </Box>
         </Box>
     );
