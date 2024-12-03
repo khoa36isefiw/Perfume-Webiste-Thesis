f69aa6d44558170687402a97a6edc1e0457eaa4b
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-12-03 23:27:52 +0700

    feat: get data from api and show it on

--- client/src/components/AdminDashboard/TopProductsSale.js
+++ client/src/components/AdminDashboard/TopProductsSale.js
@@ -1,75 +1,53 @@
 import { Avatar, Box, Divider } from '@mui/material';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     AdminHeadingTypography,
     AdminTypography,
 } from '../CustomizeTypography/CustomizeTypography';
 import { converToVND } from '../convertToVND/convertToVND';
+import useTopProductSold from '../../api/useTopProductSold';
+import { mobileScreen } from '../../Theme/Theme';
 
 const dataTitleTable = ['Name', 'Price', 'Units Sold'];
 
-const listProducts = [
-    {
-        id: 1,
-        image: 'https://res.cloudinary.com/dxulhqdp3/image/upload/v1724161759/perfumes/men/Homme_Intense_zw7zee.png',
-        name: 'Luna Kei',
-        price: 2133000,
-
-        unitsSold: 3,
-    },
-    {
-        id: 2,
-        image: 'https://res.cloudinary.com/dxulhqdp3/image/upload/v1724161758/perfumes/men/Allure_Homme_Sport_wtevx6.png',
-        name: 'Luna Kei',
-        price: 2678000,
-
-        unitsSold: 23,
-    },
-    {
-        id: 3,
-        image: 'https://res.cloudinary.com/dxulhqdp3/image/upload/v1724161759/perfumes/men/Allure_Homme_Sport_Eau_Extreme_sefblt.png',
-        name: 'Luna Kei',
-        price: 1121000,
-
-        unitsSold: 13,
-    },
-    {
-        id: 4,
-        image: 'https://res.cloudinary.com/dxulhqdp3/image/upload/v1724161758/perfumes/men/Sauvage_EDT_vucd51.png',
-        name: 'Luna Kei',
-        price: 2563000,
-
-        unitsSold: 24,
-    },
-    {
-        id: 5,
-        image: 'https://res.cloudinary.com/dxulhqdp3/image/upload/v1724161759/perfumes/men/Homme_Intense_zw7zee.png',
-        name: 'Luna Kei',
-        price: 1782000,
-
-        unitsSold: 25,
-    },
-    {
-        id: 6,
-        image: 'https://res.cloudinary.com/dxulhqdp3/image/upload/v1724161758/perfumes/men/Allure_Homme_Sport_wtevx6.png',
-        name: 'Luna Kei',
-        price: 12378000,
-
-        unitsSold: 12,
-    },
-];
-
 function TopProductsSale() {
+    const { data: productSold, isLoading } = useTopProductSold();
+    console.log('recentTransactions: ', productSold?.data);
+    const [topProductSold, setTopProductSold] = useState([]);
+    //return an array object
+
+    useEffect(() => {
+        if (productSold && productSold?.data) {
+            const listUsers = productSold?.data?.map((product) => {
+                return {
+                    id: product._id,
+                    image: product.imagePath[0],
+                    name: product.nameEn,
+                    price: product.variants[0].price,
+                    unitsSold: product.unitsSold,
+                };
+            });
+            setTopProductSold(listUsers);
+        }
+    }, [productSold?.data]);
+    console.log('topProductSold: ', topProductSold);
+
     return (
         <Box sx={{ padding: 2, borderRadius: 1, bgcolor: '#fff', border: '1px solid #d5d5d5' }}>
             <AdminHeadingTypography sx={{ fontSize: '24px', mb: 2 }}>
                 Top Products by Units Sold
             </AdminHeadingTypography>
 
-            {/* Table Header */}
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+            <Box
+                sx={{
+                    display: 'grid',
+                    gridTemplateColumns: '2fr 1fr 1fr',
+                    alignItems: 'center',
+                    mb: 1,
+                }}
+            >
                 {dataTitleTable.map((title, index) => (
-                    <AdminTypography key={index} sx={{ flex: 1, textAlign: 'center' }}>
+                    <AdminTypography key={index} sx={{ flex: 1, textAlign: 'left' }}>
                         {title}
                     </AdminTypography>
                 ))}
@@ -80,51 +58,67 @@ function TopProductsSale() {
 
             {/* Table Body */}
             <Box sx={{ maxHeight: '270px', overflow: 'scroll' }}>
-                {listProducts.map((product, index) => (
-                    <Box key={product.id}>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'space-between',
-                                mb: 1,
-                            }}
-                        >
+                {topProductSold.length > 0 &&
+                    topProductSold.map((product, index) => (
+                        <Box key={product.id}>
                             <Box
                                 sx={{
-                                    display: 'flex',
+                                    display: 'grid',
+                                    gridTemplateColumns: '2fr 1fr 1fr',
                                     alignItems: 'center',
-                                    justifyContent: 'center',
-                                    px: 3,
+                                    mb: 1,
                                 }}
                             >
-                                <Avatar
-                                    src={product.image}
-                                    alt="Best Products "
+                                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                    <Avatar
+                                        src={product.image}
+                                        alt="Best Products"
+                                        sx={{
+                                            height: '40px',
+                                            width: '40px',
+                                            borderRadius: 1,
+                                            bgcolor: '#ccc',
+                                        }}
+                                    />
+                                    <AdminTypography
+                                        sx={{
+                                            ml: 2,
+                                            textAlign: 'left',
+                                            width: '100px',
+                                            textTransform: 'capitalize',
+                                            [mobileScreen]: {
+                                                ml: 1,
+                                                fontSize: '13px',
+                                            },
+                                        }}
+                                    >
+                                        {product.name}
+                                    </AdminTypography>
+                                </Box>
+
+                                <AdminTypography
                                     sx={{
-                                        height: '40px',
-                                        width: '40px',
-                                        borderRadius: 1,
-                                        bgcolor: '#ccc',
+                                        textAlign: 'left',
+                                        pr: 2,
                                     }}
-                                />
-                                <AdminTypography sx={{ flex: 1, ml: 2 }}>
-                                    {product.name}
+                                >
+                                    {converToVND(product.price)}
+                                </AdminTypography>
+
+                                <AdminTypography
+                                    sx={{
+                                        textAlign: 'center',
+                                    }}
+                                >
+                                    {product.unitsSold}
                                 </AdminTypography>
                             </Box>
-                            <AdminTypography sx={{ flex: 1, textAlign: 'center' }}>
-                                {converToVND(product.price)}
-                            </AdminTypography>
 
-                            <AdminTypography sx={{ flex: 1, textAlign: 'center' }}>
-                                {product.unitsSold}
-                            </AdminTypography>
+                            <React.Fragment>
+                                {topProductSold.length - 1 !== index && <Divider sx={{ my: 1 }} />}
+                            </React.Fragment>
                         </Box>
-                        <React.Fragment>
-                            {listProducts.length - 1 !== index && <Divider sx={{ my: 1 }} />}
-                        </React.Fragment>
-                    </Box>
-                ))}
+                    ))}
             </Box>
         </Box>
     );
