9049eb2461491c95a0ac5c3cb2c95230c43c159c
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-12-04 23:53:08 +0700

    feat: add statistics api

--- client/src/api/adminAPI.js
+++ client/src/api/adminAPI.js
@@ -11,4 +11,24 @@ export const adminAPI = {
         const url = `/admin/revenue?year=2024`;
         return axiosClient.get(url);
     },
+
+    getStatisticUser: (timeFrame) => {
+        const url = `/admin/user?timeframe=${timeFrame}`;
+        return axiosClient.get(url);
+    },
+
+    getStatisticOrder: (timeFrame) => {
+        const url = `/admin/order?timeframe=${timeFrame}`;
+        return axiosClient.get(url);
+    },
+
+    getStatisticProduct: (timeFrame) => {
+        const url = `/admin/product?timeframe=${timeFrame}`;
+        return axiosClient.get(url);
+    },
+
+    getStatisticReview: (timeFrame) => {
+        const url = `/admin/review?timeframe=${timeFrame}`;
+        return axiosClient.get(url);
+    },
 };
