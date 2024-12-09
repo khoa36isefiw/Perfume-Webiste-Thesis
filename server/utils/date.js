261c424acce5b1d8c7a618e8908848cc78062ee4
thoaihuynh0109 <thoaifamily83@gmail.com>
2024-12-09 22:58:43 +0700

    re handle get day range

--- server/utils/date.js
+++ server/utils/date.js
@@ -1,19 +1,10 @@
 const getDateRange = (timeframe) => {
     const now = new Date();
-    const dayOfWeek = now.getDay(); // Sunday is 0, Monday is 1, ..., Saturday is 6
-    const startOfCurrentWeek = new Date(now);
-    startOfCurrentWeek.setDate(now.getDate() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1)); // Adjust for Monday start
 
     if (timeframe === 'week') {
-        const startOfLastWeek = new Date(startOfCurrentWeek);
-        startOfLastWeek.setDate(startOfLastWeek.getDate() - 7); // Go back 7 days
-        const endOfLastWeek = new Date(startOfLastWeek);
-        endOfLastWeek.setDate(endOfLastWeek.getDate() + 6); // Add 6 days for the end of the week
-        return { startDate: startOfLastWeek, endDate: endOfLastWeek };
-    }
-
-    // Other timeframes (day, month)
-    if (timeframe === 'day') {
+        const startDate = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 7);
+        return { startDate, endDate: now };
+    } else if (timeframe === 'day') {
         const startDate = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1);
         return { startDate, endDate: now };
     } else if (timeframe === 'month') {
