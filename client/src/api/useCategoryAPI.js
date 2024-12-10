526a6d938ed831e85ad3db733b43cc6a6e576536
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-12-10 19:51:45 +0700

    feat: define api get category

---
+++ client/src/api/useCategoryAPI.js
@@ -0,0 +1,15 @@
+import useSWR from 'swr';
+import axiosClient from './axiosClient';
+
+export default function useCategory() {
+    const url = '/brands';
+    const fetcher = (url) => axiosClient.get(url);
+    const { data, mutate, isLoading, error } = useSWR(url, fetcher);
+
+    return {
+        data,
+        mutate,
+        isLoading,
+        error,
+    };
+}
