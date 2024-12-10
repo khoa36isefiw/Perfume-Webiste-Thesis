6635a41bbadaf4a7a5c5afb40aa0160297acea3f
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-12-10 19:52:23 +0700

    feat: render and translate category

--- client/src/components/CategoryFilter/CategoryFilter.js
+++ client/src/components/CategoryFilter/CategoryFilter.js
@@ -3,8 +3,17 @@ import React from 'react';
 import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
 import { tabletScreen, theme } from '../../Theme/Theme';
 import { blue } from '@mui/material/colors';
+import useCategory from '../../api/useCategory';
+import { useTranslation } from 'react-i18next';
 
 function CategoryFilter() {
+    const { data: categoryData, isLoading, mutate } = useCategory();
+    const handleSelect = (category) => {
+        console.log('category: ', category.nameEn);
+        console.log('category_id: ', category._id);
+    };
+    console.log('categoryData: ', categoryData?.data);
+    const { t, i18n } = useTranslation('translate');
     return (
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <CustomizeTypography
@@ -12,61 +21,29 @@ function CategoryFilter() {
             >
                 Categories
             </CustomizeTypography>
-
-            <Button
-                sx={{
-                    py: 1,
-                    borderRadius: '8px',
-                    fontSize: '16px',
-                    fontWeight: 'bold',
-                    textTransform: 'initial',
-                    color: 'white',
-                    '&:hover': {
-                        bgcolor: theme.palette.background.thirth,
-                    },
-                    [tabletScreen]: {
-                        fontSize: '13px',
-                    },
-                }}
-            >
-                Men
-            </Button>
-            <Button
-                sx={{
-                    py: 1,
-                    borderRadius: '8px',
-                    fontSize: '14px',
-                    fontWeight: 'bold',
-                    textTransform: 'initial',
-                    color: 'white',
-                    '&:hover': {
-                        bgcolor: theme.palette.background.thirth,
-                    },
-                    [tabletScreen]: {
-                        fontSize: '13px',
-                    },
-                }}
-            >
-                Women
-            </Button>
-            <Button
-                sx={{
-                    py: 1,
-                    borderRadius: '8px',
-                    fontSize: '14px',
-                    fontWeight: 'bold',
-                    textTransform: 'initial',
-                    color: 'white',
-                    '&:hover': {
-                        bgcolor: theme.palette.background.thirth,
-                    },
-                    [tabletScreen]: {
-                        fontSize: '13px',
-                    },
-                }}
-            >
-                Recent Added
-            </Button>
+            {categoryData?.data.map((category) => (
+                <Button
+                    key={category._id}
+                    sx={{
+                        py: 1,
+                        borderRadius: '8px',
+                        fontSize: '16px',
+                        fontWeight: 'bold',
+                        textTransform: 'initial',
+                        color: 'white',
+                        '&:hover': {
+                            bgcolor: theme.palette.background.thirth,
+                        },
+                        [tabletScreen]: {
+                            fontSize: '13px',
+                        },
+                    }}
+                    onClick={() => handleSelect(category)}
+                >
+                    {/* {i18n.language === 'en' ? category.nameEn === '' ? '' : ''} */}
+                    {i18n.language === 'en' ? category.nameEn : category.nameVn}
+                </Button>
+            ))}
         </Box>
     );
 }
