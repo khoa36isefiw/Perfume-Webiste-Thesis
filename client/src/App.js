4674dc489fa43b0673c0f2276b372fc555a8545e
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-12-14 10:25:17 +0700

    feat: define logic router for guest who no log in

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,18 @@ import { Box } from '@mui/material';
 import { privateRoutes, publicRoutes } from './routes/routes';
 import UserLayouts from './layouts/UserLayout/UserLayouts';
 import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 function App() {
     // default language for the website is English
+    const { t, i18n } = useTranslation();
     useEffect(() => {
         if (window.location.pathname === '/') {
             window.location.replace('/en');
         }
     }, []);
+
+    const userData = JSON.parse(localStorage.getItem('user_data')) || {};
 
     return (
         <Box sx={{ bgcolor: '#000000' }}>
@@ -40,29 +44,28 @@ function App() {
                         const Layout = route.layout || UserLayouts;
 
                         return (
-                            // <Route
-                            //     key={route.path}
-                            //     path={route.path}
-                            //     element={
-                            //         <Navigate to="/signin" />
-                            //         user.user ? ( // for login
-                            //             <Layout>
-                            //                 <Page />
-                            //             </Layout>
-                            //         ) : (
-                            //             <Navigate to="/signin" />
-                            //         )
-                            //     }
-                            // />
                             <Route
                                 key={route.path}
                                 path={route.path}
                                 element={
-                                    <Layout>
-                                        <Page />
-                                    </Layout>
+                                    userData.user ? ( // for login
+                                        <Layout>
+                                            <Page />
+                                        </Layout>
+                                    ) : (
+                                        <Navigate to={`/${i18n.language}/sign-in`} />
+                                    )
                                 }
                             />
+                            // <Route
+                            //     key={route.path}
+                            //     path={route.path}
+                            //     element={
+                            //         <Layout>
+                            //             <Page />
+                            //         </Layout>
+                            //     }
+                            // />
                         );
                     })}
                 </Routes>
